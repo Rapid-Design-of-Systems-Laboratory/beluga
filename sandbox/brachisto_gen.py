9ae4bf33d4d29f7da90c0c910e8f56c9da2f5f64
Thomas Antony <tantony@purdue.edu>
2015-05-28 14:08:56 -0500

    Added run_continuation() function to brachisto_gen.py

--- sandbox/brachisto_gen.py
+++ sandbox/brachisto_gen.py
@@ -77,57 +77,52 @@ solver = SingleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=10
 #           Stuff in the input file                            #
 ################################################################
 from continuation import *
-
-bvp = bs.Problem(problem_mod.deriv_func,problem_mod.bc_func,
-                                states = ['x','y','v','lamX','lamY','lamV','tf'],
-                                initial_bc  = {'x':0.0, 'y':0.0, 'v':1.0},
-                                terminal_bc = {'x':0.1, 'y':-0.1}, 
-                                const = {'g':-9.81},
-                                constraint = {}
-                                )
+def run_continuation():
+    bvp = bs.Problem(problem_mod.deriv_func,problem_mod.bc_func,
+                                    states = ['x','y','v','lamX','lamY','lamV','tf'],
+                                    initial_bc  = {'x':0.0, 'y':0.0, 'v':1.0},
+                                    terminal_bc = {'x':0.1, 'y':-0.1}, 
+                                    const = {'g':-9.81},
+                                    constraint = {}
+                                    )
                                 
-# step1 will actually be loaded from an array of continuation steps
-step1 = ContinuationStep()
-# 10 seconds for 100 in 5 steps
-# 20 seconds for 100 in 25 steps
-# 3 seconds for 100 in 2 steps
-step1.num_cases = 2
-step1.terminal('x',5.0)
-step1.terminal('y',-5.0)
+    # step1 will actually be loaded from an array of continuation steps
+    step1 = ContinuationStep()
+    # 10 seconds for 100 in 5 steps
+    # 20 seconds for 100 in 25 steps
+    # 3 seconds for 100 in 2 steps
+    step1.num_cases = 2
+    step1.terminal('x',100.0)
+    step1.terminal('y',-100.0)
 
-step1.set_bvp(bvp)
-step1.reset();
+    step1.set_bvp(bvp)
+    step1.reset();
 
+    print('\nRunning continuation set 1:')
+    sol_last = solinit
+    total_time = 0.0;
+    plt.clf()
+    while not step1.complete():
+        print('Starting iteration '+str(step1.ctr+1)+'/'+str(step1.num_cases))
+        tic()
+        bvp = step1.next()
+        sol = solver.solve(bvp,sol_last)
 
-# from bokeh.plotting import figure, output_file, show
-# output_file("brachisto.html", title="Solution for Brachistochrone problem")
-# p = figure(title="Solution for Brachistochrone problem", x_axis_label='x', y_axis_label='y')
+        # Update solution for next iteration
+        sol_last = sol
+        elapsed_time = toc()
+        total_time  += elapsed_time
+        print('Iteration %d/%d converged in %0.4f seconds\n' % (step1.ctr, step1.num_cases, elapsed_time))
 
+        plt.plot(sol.y[0,:], sol.y[1,:],'-')
+        # p.line(sol.y[0,:], sol.y[1,:], line_width=2, color=tuple(x*255 for x in RGB_tuples[step1.ctr-1]))
 
-print('\nRunning continuation set 1:')
-sol_last = solinit
-total_time = 0.0;
-plt.clf()
-while not step1.complete():
-    print('Starting iteration '+str(step1.ctr+1)+'/'+str(step1.num_cases))
-    tic()
-    bvp = step1.next()
-    sol = solver.solve(bvp,sol_last)
+    print('Continuation process completed in %0.4f seconds.\n' % total_time)
+    ###############################################################
+    # show(p)   # For bokeh
+    plt.title('Solution for Brachistochrone problem')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show(block=False)
 
-    # Update solution for next iteration
-    sol_last = sol
-    elapsed_time = toc()
-    total_time  += elapsed_time
-    print('Iteration %d/%d converged in %0.4f seconds\n' % (step1.ctr, step1.num_cases, elapsed_time))
-
-    plt.plot(sol.y[0,:], sol.y[1,:],'-')
-    # p.line(sol.y[0,:], sol.y[1,:], line_width=2, color=tuple(x*255 for x in RGB_tuples[step1.ctr-1]))
-
-print('Continuation process completed in %0.4f seconds.\n' % total_time)
-###############################################################
-# show(p)   # For bokeh
-plt.title('Solution for Brachistochrone problem')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show(block=False)
-
+run_continuation()
