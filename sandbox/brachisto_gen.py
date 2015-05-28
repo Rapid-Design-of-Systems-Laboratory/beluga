57b07e2ebf7e0a11987ca84fd97d0a8f7f3963ec
Thomas Antony <tantony@purdue.edu>
2015-05-28 12:36:56 -0500

    Added some stuff to try out 'bokeh'

--- sandbox/brachisto_gen.py
+++ sandbox/brachisto_gen.py
@@ -26,15 +26,15 @@ problem_data = {
      ],
      'num_states': 7,
      'left_bc_list':[
-         "_ya[0] - _x0['x']", # x(0
+         "_ya[0] - _x0['x']", # x(0)
          "_ya[1] - _x0['y']", # y(0)
-         "_ya[2] - _x0['v']"         
+         "_ya[2] - _x0['v']"  # v(0)       
      ],
      'right_bc_list':[
          "_yb[0] - _xf['x']", # x(tf)
          "_yb[1] - _xf['y']", # y(tf)
-         "_yb[5] + 0.0",   # lamV(tf)
-         "_H     - 0",     # H(tf)
+         "_yb[5] + 0.0",      # lamV(tf)
+         "_H     - 0",        # H(tf)
      ],
      'control_options':[
          [{'name':'thetta','expr':'-acos(-((lamX*v)/sqrt(g**2*lamV**2+2*g*lamV*lamY*v+(lamX**2+lamY**2)*v**2)))'}],
@@ -89,12 +89,18 @@ step1 = ContinuationStep()
 # 10 seconds for 100 in 5 steps
 # 20 seconds for 100 in 25 steps
 # 3 seconds for 100 in 2 steps
-step1.num_cases = 2
+step1.num_cases = 5
 step1.terminal('x',20.0)
 step1.terminal('y',-20.0)
 
 step1.set_bvp(bvp)
 step1.reset();
+
+
+# from bokeh.plotting import figure, output_file, show
+# output_file("brachisto.html", title="Solution for Brachistochrone problem")
+# p = figure(title="Solution for Brachistochrone problem", x_axis_label='x', y_axis_label='y')
+
 
 print('\nRunning continuation set 1:')
 sol_last = solinit
@@ -113,10 +119,11 @@ while not step1.complete():
     print('Iteration %d/%d converged in %0.4f seconds\n' % (step1.ctr, step1.num_cases, elapsed_time))
     
     plt.plot(sol.y[0,:], sol.y[1,:],'-')
+    # p.line(sol.y[0,:], sol.y[1,:], line_width=2, color=tuple(x*255 for x in RGB_tuples[step1.ctr-1]))
 
 print('Continuation process completed in %0.4f seconds.\n' % total_time)
 ################################################################
-
+# show(p)   # For bokeh
 plt.title('Solution for Brachistochrone problem')
 plt.xlabel('x')
 plt.ylabel('y')
