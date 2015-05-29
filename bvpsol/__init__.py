918e21702a3f7ec66f43026877ef7d5dea49a37c
Thomas Antony <tantony@purdue.edu>
2015-05-29 15:05:21 -0500

    Removed redundant files

--- bvpsol/__init__.py
+++ bvpsol/__init__.py
@@ -1,7 +1,7 @@
 from .Solution import Solution
 from .bvpinit import bvpinit
 from .BVP import BVP
-from .FunctionTemplate import FunctionTemplate
+# from .FunctionTemplate import FunctionTemplate
 from .Algorithm import Algorithm
 
 # __all__ = ['Algorithm','Solution','BVP','FunctionTemplate','bvpinit']
