053172b6983af1d0bab46126530bd05911687461
Thomas Antony <tantony@purdue.edu>
2015-06-26 14:16:32 -0500

    Organized code a little better in Scaling

--- beluga/optim/Scaling.py
+++ beluga/optim/Scaling.py
@@ -19,37 +19,22 @@ class Scaling(dict):
         self.units[unit_str] = unit_scale
         return self
 
-    def initialize(self, nec_cond):
+    def initialize(self, problem, problem_data):
         """Initializes the scaling process"""
         self.scale_val = {}
-        self.problem_data = nec_cond.problem_data
-
-        for unit,scale in self.units.items():
-            pass
-            # if isinstance(scale,num.Number):
-            #     scale_func[unit] =
-                # Custom scaling factor
-            # print((unit,scale,isinstance(scale,num.Number)))
+        self.problem_data = problem_data
 
         # Generate scaling functions for states, costates
         # constants, constraints, lagrange multipliers
 
-        problem = nec_cond.problem
-
         # Convert units to symbols
-        units_sym = symbols(list(self.units))
+        self.units_sym = symbols(list(self.units))
 
         # Growing list TODO: Put inside utils
-        # Make sympify2 ?
-
         # TODO: Automate the following sections
 
         # Scaling functions for constants
-        # self.scale_func['const'] = {}
-        # for const in problem.constants():
-        #     self.scale_func['const'][str(const)] = lambdify(units_sym,sympify2(const.unit))
-
-        self.scale_func['const'] = {str(const): self.create_scale_fn(units_sym,const.unit)
+        self.scale_func['const'] = {str(const): self.create_scale_fn(const.unit)
                                     for const in problem.constants()}
 
         # Cost function used for scaling costates
@@ -58,21 +43,18 @@ class Scaling(dict):
             raise ValueError('At least one cost function must be specified as nonzero!')
         cost_unit = problem.cost[cost_used[0]].unit
 
-
         # Scaling functions for states & costates
         self.scale_func['states'] = {}
-        for state in problem.states():
-            self.scale_func['states'][str(state)] = lambdify(units_sym,sympify2(state.unit))
-
-            costate = state.make_costate();
-            costate_scale = '('+cost_unit+')/('+state.unit+')'
-            self.scale_func['states'][costate] = lambdify(units_sym,sympify2(costate_scale))
+        self.scale_func['states'] = {str(state): self.create_scale_fn(state.unit)
+                            for state in problem.states()}
+        self.scale_func['states'].update({ state.make_costate():
+                            self.create_scale_fn('('+cost_unit+')/('+state.unit+')')
+                            for state in problem.states()})
 
         # Scaling function for the independent variable
         # TODO: Fix hardcoding
         # self.scale_func['independent_var'] = lambdify(units_sym,sympify2(problem.indep_var().unit))
-        self.scale_func['states']['tf'] = lambdify(units_sym,sympify2(problem.indep_var().unit))
-        # self.scale_func['states']['tf'] = self.create_scale_fn(units_sym,problem.indep_var().unit)
+        self.scale_func['states']['tf'] = self.create_scale_fn(problem.indep_var().unit)
 
         self.scale_func['initial'] = self.scale_func['states']
         self.scale_func['terminal'] = self.scale_func['states']
@@ -86,51 +68,62 @@ class Scaling(dict):
 
             mul_var  = c.make_multiplier(indices[c.type])
             mul_unit = '('+cost_unit+')/('+c.unit+')'
-            self.scale_func['parameters'][mul_var] = lambdify(units_sym, sympify2(mul_unit))
+            self.scale_func['parameters'][mul_var] = self.create_scale_fn(mul_unit)
             indices[c.type] += 1 # increment multiplier index
 
-    def create_scale_fn(self,units_sym,unit_expr):
-        return lambdify(units_sym,sympify2(unit_expr))
-        pass
+    def create_scale_fn(self,unit_expr):
+        return lambdify(self.units_sym,sympify2(unit_expr))
+
+    def compute_base_scaling(self,bvp,sol,scale_expr):
+        if isinstance(scale_expr,num.Number):
+            # If scaling factor is a number, use it
+            return scale_expr
+        else:
+            # If it is an expression, evaluate it
+            # Setup environment to evaluate expression
+            # Add list of states, costates and time and their peak values
+            variables  = [(state,max(abs(sol.y[idx,:])))
+                            for idx,state in enumerate(self.problem_data['state_list'])]
+
+            # Add auxiliary variables and their values (hopefully they dont clash)
+            variables += [(var,bvp.aux_vars[aux['type']][var])
+                            for aux in self.problem_data['aux_list']
+                            for var in aux['vars']
+                            if aux['type'] not in Scaling.excluded_aux]
+            var_dict = dict(variables)
+
+            # Evaluate expression to get scaling factor
+            return float(sympify2(scale_expr).subs(var_dict,dtype=float).evalf())
+
     def compute_scaling(self,bvp,sol):
         from collections import OrderedDict
         # Units should be stored in order to be used as function arguments
-        self.scale_factors = OrderedDict()   # Scaling factors for each unit
-        for unit,scale_expr in self.units.items():
-            if isinstance(scale_expr,num.Number):
-                # If scaling factor is a number, use it
-                self.scale_factors[unit] = scale_expr
-            else:
-                # If it is an expression, evaluate it
-                # Setup environment to evaluate expression
-                # Add list of states, costates and time and their peak values
-
-                variables  = [(state,max(abs(sol.y[idx,:])))
-                                for idx,state in enumerate(self.problem_data['state_list'])]
-
-                # Add auxiliary variables and their values (hopefully they dont clash)
-                variables += [(var,bvp.aux_vars[aux['type']][var])
-                                for aux in self.problem_data['aux_list']
-                                for var in aux['vars']
-                                if aux['type'] not in Scaling.excluded_aux]
-                var_dict = dict(variables)
-
-                # Evaluate expression to get scaling factor
-                self.scale_factors[unit] = float(sympify2(scale_expr).subs(var_dict,dtype=float).evalf())
+        self.scale_factors = OrderedDict()
+        # Evaluate scaling factors for each base unit
+        self.scale_factors.update({unit: self.compute_base_scaling(bvp,sol,scale_expr)
+                             for unit,scale_expr in self.units.items()})
 
         # Ordered list of unit scaling factors for use as function parameters
         scale_factor_list = [v for (k,v) in self.scale_factors.items()]
 
         # Find scaling factors for each entity in problem
         self.scale_vals = {}
+        # Dictionary comprehension version -- remove for lack of readability
+        # self.scale_vals = {var_type: var_funcs(*self.scale_factors)
+        #                     if callable(var_funcs)
+        #                     else {
+        #                         var_name: var_func(*scale_factor_list)
+        #                         for var_name,var_func in var_funcs.items()
+        #                     }
+        #                     for var_type,var_funcs in self.scale_func.items() }
 
         for var_type,var_funcs in self.scale_func.items():
             # If there are no sub items, use the scale factor directly
             if callable(var_funcs):
                 self.scale_vals[var_type] = var_funcs(*self.scale_factors)
             else:
-                self.scale_vals[var_type] = {}
                 # Else call scaling function for each sub item
+                self.scale_vals[var_type] = {}
                 for var_name,var_func in var_funcs.items():
                     self.scale_vals[var_type][var_name] = var_func(*scale_factor_list)
 
