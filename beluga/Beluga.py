6f152f8398273e5977e222d826fe24a2404596ae
Thomas Antony <tantony@purdue.edu>
2015-06-26 17:44:40 -0500

    Fixed major bug in Scaling!!

--- beluga/Beluga.py
+++ beluga/Beluga.py
@@ -13,18 +13,18 @@ from beluga.bvpsol import algorithms
 import dill
 
 class Beluga(object):
-    __metaclass__ = SingletonMetaClass
+    # __metaclass__ = SingletonMetaClass
     version = '0.1'
     _THE_MAGIC_WORD = object()
     instance = None
 
     config = BelugaConfig().config # class variable globally accessible
-    def __init__(self,problem,token,input_module=None):
+    def __init__(self,problem,token=None,input_module=None):
         self.problem = problem
         self.input_module = input_module
 
-        if token is not self._THE_MAGIC_WORD:
-            raise ValueError("Don't construct directly, use create() or run()")
+        # if token is not self._THE_MAGIC_WORD:
+        #     raise ValueError("Don't construct directly, use create() or run()")
 
     @classmethod
     def run(cls,problem):
@@ -47,7 +47,7 @@ class Beluga(object):
 
         # TODO: Get default solver options from configuration or a defaults file
         if problem.bvp_solver is None:
-            problem.bvp_solver = algorithms.SingleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=1000, verbose = False)
+            problem.bvp_solver = algorithms.SingleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=1000, verbose = True, cached = False)
 
         # Set the cache directory to be in the current folder
         cache_dir = os.getcwd()+'/_cache'
@@ -57,7 +57,6 @@ class Beluga(object):
         except:
             pass
         problem.bvp_solver.set_cache_dir(cache_dir)
-
 
         if isinstance(problem,Problem):
             # Create instance of Beluga class
