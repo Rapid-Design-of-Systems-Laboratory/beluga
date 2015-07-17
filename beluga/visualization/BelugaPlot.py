e5a723b2c93edd9683f83465e1614098eb63f3d0
Thomas Antony <tantony@purdue.edu>
2015-07-17 15:52:29 -0500

    Added expression evaluation

--- beluga/visualization/BelugaPlot.py
+++ beluga/visualization/BelugaPlot.py
@@ -1,16 +1,22 @@
 from beluga.visualization.elements import PlotList
+from beluga.visualization.renderers import *
 
 class BelugaPlot:
     """
     Manages the plotting framework
     """
-    def __init__(self, filename='data.dill'):
+    def __init__(self, filename='data.dill', renderer = None):
         """
         Initializes plotting framework with given data file
         """
         self._plots = PlotList()
         self._plotconfig = {}
         self.global_settings = {}
+        # TODO: Get default renderer information from global configuration
+        if renderer is None:
+            self.renderer = MatPlotLibRenderer()
+        else:
+            self.renderer = renderer
 
     def add_plot(self, plot=None):
         """
