b2c11cf1cb3bd8ac53bc4a9d74641b8f0b5a17f0
Thomas Antony <tantony@purdue.edu>
2015-07-18 14:19:08 -0500

    Fixed bug in saving data

--- examples/planarHypersonic_plot.py
+++ examples/planarHypersonic_plot.py
@@ -1,11 +1,17 @@
 from beluga.visualization import BelugaPlot
 
-plots = BelugaPlot('/Users/tantony/dev/mjgrant-beluga/examples/data.dill')
+plots = BelugaPlot('/Users/tantony/dev/mjgrant-beluga/examples/data.dill',default_sol=1,default_iter=20)
 
-plots.add_plot().xlabel('v (km/s)')     \
-                .ylabel('h (km)')       \
-                .x('v/1000')            \
-                .y('h/1000')            \
+plots.add_plot().x('t','t (s)')                  \
+                .y('h/1000','h (km)')            \
+                .title('Altitude vs. Time')
+
+plots.add_plot().x('theta*re/1000','Downrange (km)')                  \
+                .y('h/1000','h (km)')            \
+                .title('Altitude vs. Downrange')
+
+plots.add_plot().x('v/1000','v (km/s)')          \
+                .y('h/1000','h (km)')            \
                 .title('Altitude vs. Velocity')
 
 plots.render()
