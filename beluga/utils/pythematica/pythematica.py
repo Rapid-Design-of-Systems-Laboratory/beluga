b999f64896e7f05a98537416d625d2ae1118ab51
Thomas Antony <tantony@purdue.edu>
2015-10-04 21:04:12 -0400

    Mathematica rootsolving seems to work for now.

--- beluga/utils/pythematica/pythematica.py
+++ beluga/utils/pythematica/pythematica.py
@@ -1,5 +1,6 @@
-import subprocess
+import subprocess, re
 from sympy import mathematica_code as mcode
+from beluga.utils import sympify2
 # Credits: http://sapiensgarou.blogspot.com.br/2012/06/how-to-run-mathematica-functions-on.html
 def mathematica_run(command):
     """Call the shell script which in turn calls mathematica"""
@@ -12,24 +13,57 @@ def mathematica_run(command):
     out, err = p.communicate()
     return out
 
+def mathematica_subs(expr):
+    rules = (
+        (r'\[','('),   # Replace square brackets with parenthesis
+        (r'\]',')'),   # Replace square brackets with parenthesis
+        (r'Arc(\w+)','a\\1'),         # Replace inverse trig functions
+    )
+    for rule,replacement in rules:
+        expr,n = re.subn(rule,replacement,expr)
+
+    return expr
+
 def mathematica_solve(expr,vars):
     if isinstance(expr,list):
-        m_expr = mcode([mcode(e)+' == 0' for e in expr])
+        m_expr = mcode(['Exp[dummyfoobar]*('+mcode(e)+') == 0' for e in expr])
     else:
-        m_expr = mcode(expr)+' == 0'
+        m_expr = 'Exp[dummyfoobar]*('+mcode(expr)+') == 0'
 
-    cmd = 'Solve[%s,%s]' % (m_expr,mcode(vars))
+    cmd = 'Quiet[Simplify[Solve[%s,%s]]]' % (m_expr,mcode(vars)) # Suppress warnings for now
     sol_str = mathematica_run(cmd).decode('utf-8').strip()
-    out =  [dict([varsol.split(' -> ') for varsol in s.split(', ')])
+    if sol_str == '{}' or "Solve[" in sol_str:  # No solution found
+        return []
+    else:
+        # Convert solution to  dictionary
+        out = [dict([varsol.split(' -> ') for varsol in s.split(', ')])
                 for s in sol_str[2:-2].split('}, {')]
-    return out
+        # Convert solution strings to sympy expressions
+        out = [dict([(var,sympify2(mathematica_subs(expr))) for (var,expr) in sol.items()]) for sol in out]
+        print(out)
+        return out
 
 if __name__ == '__main__':
     from beluga.utils import pythematica
     from sympy import sympify
 
-    print(pythematica.mathematica_solve(sympify('a - 5'),sympify('a')))
-    expr = [sympify('a + b - 5'),sympify('a - b + 10')]
-    v = [sympify('a'),sympify('b')]
-    print(pythematica.mathematica_solve(expr,v))
-    print(pythematica.mathematica_solve(sympify('x^2 - 2*x + 3'),sympify('x')))
+    # print(pythematica.mathematica_subs('-ArcCos[-((lX*v)/Sqrt[g^2*lV^2 - 2*g*lV*lY*v + (lX^2 + lY^2)*v^2])]'))
+    sol_str = '{{theta -> -ArcCos[-((lX*v)/Sqrt[g^2*lV^2 - 2*g*lV*lY*v + (lX^2 + lY^2)*v^2])]}, {theta -> ArcCos[-((lX*v)/Sqrt[g^2*lV^2 - 2*g*lV*lY*v + (lX^2 + lY^2)*v^2])]}, {theta -> -ArcCos[(lX*v)/Sqrt[g^2*lV^2 - 2*g*lV*lY*v + (lX^2 + lY^2)*v^2]]}, {theta -> ArcCos[(lX*v)/Sqrt[g^2*lV^2 - 2*g*lV*lY*v + (lX^2 + lY^2)*v^2]]}}'
+
+    out = [dict([varsol.split(' -> ') for varsol in s.split(', ')])
+        for s in sol_str[2:-2].split('}, {')]
+    out = [dict([(var,sympify2(mathematica_subs(expr))) for (var,expr) in sol.items()]) for sol in out]
+    print(out)
+
+
+    # print(pythematica.mathematica_solve(sympify('a - 5'),sympify('a')))
+    # expr = [sympify('a + b - 5'),sympify('a - b + 10')]
+    # v = [sympify('a'),sympify('b')]
+    # print(pythematica.mathematica_solve(expr,v))
+    #
+    # # Example with no solution
+    # expr = [sympify('a + b - 5'),sympify('a + b - 10')]
+    # v = [sympify('a'),sympify('b')]
+    # print(pythematica.mathematica_solve(expr,v))
+    #
+    # print(pythematica.mathematica_solve(sympify('x^2 - 2*x + 3'),sympify('x')))
