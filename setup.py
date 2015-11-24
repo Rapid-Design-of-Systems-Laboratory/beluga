681b5fc3fb04d526c7e42ca7c75f619341fef244
Thomas Antony <tantony@purdue.edu>
2015-11-24 13:32:20 -0200

    Added support for setup.py test command

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import find_packages, setup
+import os, sys
+# Run tests
+# Ref: http://www.pydanny.com/python-dot-py-tricks.html
+if sys.argv[-1] == 'test':
+    test_requirements = [
+        'pytest',
+        'pytest-cov'
+        'coverage'
+    ]
+    try:
+        modules = map(__import__, test_requirements)
+    except ImportError as e:
+        err_msg = e.message.replace("No module named ", "")
+        msg = "%s is not installed. Install your test requirments." % err_msg
+        raise ImportError(msg)
+    os.system('py.test tests')
+    sys.exit()
+
 setup(name="beluga",
       version="0.1",
       description="A trajectory optimization framework",
