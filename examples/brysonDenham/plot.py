d54af8547a9c3e7729e423ca8e085c92fe3d4fe8
Thomas Antony <tantony@purdue.edu>
2016-02-02 12:14:05 -0500

    Added bounded brysonDenham problem

--- examples/brysonDenham/plot.py
+++ examples/brysonDenham/plot.py
@@ -1,6 +1,6 @@
 from beluga.visualization import BelugaPlot
 
-plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1)
+plots = BelugaPlot('./results_0001.dill',default_sol=-1,default_step=-1)
 
 plots.add_plot().line('t','x')                    \
                 .xlabel('t (s)').ylabel('x(t)')      \
@@ -14,4 +14,11 @@ plots.add_plot().line('t','u')                    \
                 .xlabel('t (s)').ylabel('u(t)')      \
                 .title('Control history')
 
+plots.add_plot().line('t','lamXI11')                    \
+                .xlabel('t (s)').ylabel('lamX(t)')      \
+                .title('lamX vs. t')
+
+plots.add_plot().line('t','lamXI12')                    \
+                .xlabel('t (s)').ylabel('lamV(t)')      \
+                .title('lamV vs. t')
 plots.render()
