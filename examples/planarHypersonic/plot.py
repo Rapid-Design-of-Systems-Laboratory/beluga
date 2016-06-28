e60295d2f4f9590ef3c6e08ccebd5a6e557e6263
Thomas Antony <tantony@purdue.edu>
2016-06-28 01:51:29 -0400

    Normalized path constraints working

--- examples/planarHypersonic/plot.py
+++ examples/planarHypersonic/plot.py
@@ -1,30 +1,34 @@
 from beluga.visualization import BelugaPlot
 
-# plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1)
-plots = BelugaPlot('./phu_2k5_eps4.dill',default_sol=-1,default_step=-1, renderer='bokeh')
+plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1)
+# plots = BelugaPlot('./phu_2k5_eps4.dill',default_sol=-1,default_step=-1, renderer='bokeh')
 #
-plots.add_plot().line_series('theta*re/1000','h/1000', step=-1, skip=3)                    \
+# plots.add_plot().line_series('theta*re/1000','h/1000', step=-1, skip=5)                    \
+#                 .xlabel('Downrange (km)').ylabel('h (km)')      \
+#                 .title('Altitude vs. Downrange')
+
+plots.add_plot().line('theta*re/1000','h/1000')                    \
                 .xlabel('Downrange (km)').ylabel('h (km)')      \
                 .title('Altitude vs. Downrange')
 
-# plots.add_plot().line('theta*re/1000','h/1000')                    \
-#                 .xlabel('Downrange (km)').ylabel('h (km)')      \
-#                 .title('Altitude vs. Downrange')
-#
-# plots.add_plot().line('t','alfa*180/3.14')                    \
-#                 .xlabel('t (s)').ylabel('alfa (degrees)')      \
-#                 .title('Angle of attack vs. Time')
-
-plots.add_plot().line_series('v/1000','h/1000', step=-1, skip=9)                    \
+plots.add_plot().line('v/1000','h/1000')                    \
                 .xlabel('v (km/s)').ylabel('h (km)')      \
                 .title('Altitude vs. Velocity')
 
-plots.add_plot().line_series('t','alfa*180/3.14', step=-1, skip=9)                    \
-                .xlabel('t (s)').ylabel('alfa (degrees)')      \
-                .title('Angle of attack vs. Time')
-
 # plots.add_plot().line('t','alfa*180/3.14')                    \
 #                 .xlabel('t (s)').ylabel('alfa (degrees)')      \
 #                 .title('Angle of attack vs. Time')
+
+# plots.add_plot().line_series('v/1000','h/1000', step=-1, skip=5)                    \
+#                 .xlabel('v (km/s)').ylabel('h (km)')      \
+#                 .title('Altitude vs. Velocity')
+#
+# plots.add_plot().line_series('t','alfa*180/3.14', step=-1, skip=5)                    \
+#                 .xlabel('t (s)').ylabel('alfa (degrees)')      \
+#                 .title('Angle of attack vs. Time')
+
+plots.add_plot().line('t','alfa*180/3.14')                    \
+                .xlabel('t (s)').ylabel('alfa (degrees)')      \
+                .title('Angle of attack vs. Time')
 
 plots.render()
