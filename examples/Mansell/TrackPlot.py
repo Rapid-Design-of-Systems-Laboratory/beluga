f26631f05b2af7910c117667ddfd5dad889f9403
Justin Mansell <jmansell@purdue.edu>
2017-01-08 23:01:24 -0500

    Geotiff Terrain Working
    Able to perform route optimization using terrain from Geotiff files.

--- examples/Mansell/TrackPlot.py
+++ examples/Mansell/TrackPlot.py
@@ -1,14 +1,27 @@
-#==============================================================================#
-# Tests the loading AND smoothing AND coordinate conversion of Geotiff elevation
-# data. Download Geotiff elevation from:
-# http://www.cgiar-csi.org/data/srtm-90m-digital-elevation-database-v4-1
-#==============================================================================#
+#===============================================================================
+# Plots optimal route through Geotiff terrain
+#===============================================================================
 import gdal
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import interpolate
+import dill
 from math import *
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+from scipy import interpolate
 import scipy.ndimage as ndimage
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+#Load trajectory data
+f = open('data.dill','rb')
+out=dill.load(f)
+f.close()
+xsol=out['solution'][-1][-1].y[0,:]
+ysol=out['solution'][-1][-1].y[1,:]
+lamXsol=out['solution'][-1][-1].y[2,:]
+lamYsol=out['solution'][-1][-1].y[3,:]
+tf=out['solution'][-1][-1].y[4,0]
+tsol=out['solution'][-1][-1].x*tf
 
 #Load Geotiff data
 ds = gdal.Open('WL2.tif')
@@ -25,10 +38,10 @@ lons = np.linspace(lon0,lon0+dlon*ncols,ncols)
 elevation = elevation.astype(np.float) #Convert to a floating point array (important for smoothing)
 
 #Spline fit lat/lon image
+print('Generating terrain function...')
 elevation = ndimage.gaussian_filter(elevation,sigma=20,order=0) #Smooth terrain
 geo_spl=interpolate.RectBivariateSpline(lats,lons,elevation,kx=3,ky=3,s=10)
-print(lats[0])
-print(lons[0])
+
 def terrain(x,y):
     #Computes the elevation a point (x,y) in km as measured from the lower left
     #corner.
@@ -44,6 +57,8 @@ def terrain(x,y):
         return yderiv*1j*1e-30
     else:
         return geo_spl.ev(lat,lon) #[m]
+print('Terrain function ready')
+
 
 #Generate lists of X,Y coordinates
 Xcoords = np.linspace(0,(lats[-1]-lats[0])*111,floor(len(lats)/2)) #[km]
@@ -51,32 +66,27 @@ Ycoords = np.linspace(0,(lons[-1]-lons[0])*111*np.sin(lats[-1]*np.pi/180.0),floo
 
 #Plot
 Y,X = np.meshgrid(Ycoords, Xcoords)
+#X = np.flipud(X)
 terrData=terrain(X,Y) #Create the elevation data based on the spline fit
 plt.figure(1)
 plt.axis([0,Ycoords[-1],0,Xcoords[-1]])
 plt.contour(Ycoords,Xcoords,terrData)
 plt.xlabel('East (km)')
 plt.ylabel('North (km)')
+plt.plot(ysol,xsol,'r') #Plot optimal path
+plt.scatter([ysol[0],ysol[-1]],[xsol[0],xsol[-1]],75,'r')
+
+
+#Plot control
+hdg = []
+{hdg.append(np.arctan2(-lamYsol[i],-lamXsol[i])*180/np.pi) for i in range(0,len(lamXsol))}
+
+plt.figure(2)
+plt.xlabel('Time (s)')
+plt.ylabel('Heading (deg)')
+plt.plot(-tsol*10,np.array(hdg)+180)
 plt.show()
 
-
-#NOTE: v is too computationally intensive. Suggest conversion inside terrain function
-#Make a new elevation array in X and Y coordinates
-#terr_arr = np.zeros((len(Xcoords),len(Ycoords)))
-#for i,x in enumerate(Xcoords):
-#    print(i,'out of',len(Xcoords))
-#    for j,y in enumerate(Ycoords):
-#        terr_arr[i,j] = geo_spl.ev(x/111+lats[0],y/(111*np.sin(lats[-1]*np.pi/180.0))+lons[0])
-
-#Spline fit x/y image with smoothing
-#terr_spl = interpolate.RectBivariateSpline(Xcoords,Ycoords,terr_arr,kx=3,ky=3,s=10)
-
-#Plot
-#Y,X = np.meshgrid(Ycoords, Xcoords)
-#terrData=terr_spl.ev(X,Y) #Create the elevation data based on the spline fit
-#plt.figure(1)
-#plt.axis([0,Ycoords[-1],0,Xcoords[-1]])
-#plt.contour(Ycoords,Xcoords,terrData)
-#plt.xlabel('East (km)')
-#plt.ylabel('North (km)')
-#plt.show()
+#NOTE: not sure why loading the trajectory data re-runs the terrain function in
+#      Track_demo.
+#TODO: take heading from the solution vector rather than recomputing it from the costates
