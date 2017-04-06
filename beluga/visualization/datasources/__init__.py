c14d796413f6b24f0a1ea67aa22f0334535fd02a
Thomas Antony <tantony@purdue.edu>
2017-04-06 15:45:42 -0400

    Refactored visualization module

--- beluga/visualization/datasources/__init__.py
+++
@@ -1,7 +0,0 @@
-from .BaseDataSource import BaseDataSource
-from .Dill import Dill
-
-import os
-import glob
-modules = glob.glob(os.path.dirname(__file__)+"/*.py")
-__all__ = [ os.path.basename(f)[:-3] for f in modules]
