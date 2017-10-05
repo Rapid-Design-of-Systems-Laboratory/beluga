9ff86aadc6af15b71b56003619331b294d1a6761
Thomas Antony <tantony@purdue.edu>
2017-10-05 10:16:35 -0500

    Updated plot.py to show control history.

--- examples/boundedBrachistochrone/plot.py
+++ examples/boundedBrachistochrone/plot.py
@@ -8,7 +8,7 @@ plots.add_plot().line('x','y',label='Solution')               \
                 .xlabel('x(t)').ylabel('y(t)')      \
                 .title('Trajectory')
 # .line('x','-2-0.75*x',label='Constraint2',step=-1,sol=-1) \
-# plots.add_plot().line('t','theta')                    \
-#                 .xlabel('t (s)').ylabel('theta (degrees)')      \
-#                 .title('Control history')
+plots.add_plot().line('t','theta*180/3.14')                    \
+                .xlabel('t (s)').ylabel('theta (degrees)')      \
+                .title('Control history')
 plots.render()
