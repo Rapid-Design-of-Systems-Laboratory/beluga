8288507b71cf17d9312d827f4ad32fa45bc238d6
Thomas Antony <tantony@purdue.edu>
2017-10-05 10:51:15 -0500

    Planarhypersonic unconstrained - verified to be working with brysonho.py

--- examples/planarHypersonic/plot.py
+++ examples/planarHypersonic/plot.py
@@ -3,7 +3,7 @@ from beluga.visualization.datasources import Dill
 
 # plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1)
 ds = Dill('./data.dill')
-ds2 = Dill('./phu_2k5_eps4.dill')
+# ds2 = Dill('./phu_2k5_eps4.dill')
 plots = BelugaPlot(datasource=ds,default_sol=-1,default_step=-1, renderer='matplotlib')
 # plots = BelugaPlot('./phu_2k5_eps4.dill',default_sol=-1,default_step=-1, renderer='bokeh')
 #
@@ -12,9 +12,9 @@ plots = BelugaPlot(datasource=ds,default_sol=-1,default_step=-1, renderer='matpl
 #                 .title('Altitude vs. Downrange')
 
 plots.add_plot().line('theta*re/1000','h/1000',datasource=ds,label='DS1')                    \
-                .line('theta*re/1000','h/1000',datasource=ds2,label='DS2')                   \
                 .xlabel('Downrange (km)').ylabel('h (km)')      \
                 .title('Altitude vs. Downrange')
+#                .line('theta*re/1000','h/1000',datasource=ds2,label='DS2')                   \
 #
 # plots.add_plot().line('t','alfa*180/3.14')                    \
 #                 .xlabel('t (s)').ylabel('alfa (degrees)')      \
