92870db309ebcf52c63e54106797b011894e6764
Thomas Antony <tantony@purdue.edu>
2017-10-08 19:05:08 -0500

    Initial ICRM module - incomplete.

--- beluga/optimlib/icrm.py
+++ beluga/optimlib/icrm.py
@@ -5,75 +5,92 @@ Module: optimlib
 """
 
 from .brysonho import *
+import sympy as sym
+from beluga.utils import keyboard
+from beluga.problem import SymVar
+import logging
 
-def sigmoid_two_sided(x, ub, lb, slopeAtZero=1):
-    """Two-sided saturation function based on the sigmoid function."""
-    pass
+def get_satfn(var, ubound=None, lbound=None, slopeAtZero=1):
+    # var -> varible inside saturation function
+    if ubound is None and lbound is None:
+        raise ValueError('At least one bound should be specified for the constraint.')
+    if ubound == lbound:
+        raise ValueError('The upper and lower bounds should be different.')
 
+    # Symmetric bounds
+    if ubound is None:
+        # Return lower bound sat fn
+        #ubound = -lbound
+        return lbound + sym.exp(var)
+    elif lbound is None:
+        # Return upper bound sat fn
+        #lbound = -ubound
+        return ubound - sym.exp(-var)
+    else:
+        print(ubound)
+        s = 4*slopeAtZero/(ubound - lbound)
+        return ubound - ( ubound - lbound )/( 1 + sym.exp(s*var) )
 
-def sigmoid_one_sided(x, ub=None, lb=None, slopeAtZero=1):
-    """
-    One-sided saturation function based on the sigmoid function.
+def process_path_constraints(workspace):
+    states = workspace['states']
+    controls = workspace['controls']
+    constants = workspace['constants']
+    constraints = workspace['constraints']
+    derivative_fn = workspace['derivative_fn']
+    quantity_vars = workspace['quantity_vars']
+    quantity_list = workspace['quantity_list']
 
-    Only of the bounds must be defined.
-    """
-    pass
-
-def process_path_constraints(self, workspace):
-    constraints = workspace['path_constraints']
-    quantity_subs = workspace['quantity_vars']
-
-    path_cost_expr = sympify(problem.cost['path'].expr)
-    path_cost_unit = sympify(problem.cost['path'].unit)
+    path_cost_expr = workspace['path_cost'].expr
+    path_cost_unit = workspace['path_cost'].unit
     if path_cost_expr == 0:
         logging.debug('No path cost specified, using unit from terminal cost function')
-        problem.cost['path'].unit = problem.cost['terminal'].unit
-        path_cost_unit = sympify(problem.cost['terminal'].unit)
+        path_cost_expr = workspace['terminal_cost'].expr
+        path_cost_unit = workspace['terminal_cost'].unit
 
     logging.debug('Path cost is of unit: '+str(path_cost_unit))
-    time_unit = Symbol('s')
+    time_unit = workspace['indep_var'].unit
+    path_constraints = workspace['path_constraints']
 
-    for (ind,c) in enumerate(constraints):
+    eq = constraints.get('equality', [])
+    for (ind,c) in enumerate(path_constraints):
         # Determine order of constraint
-        logging.debug('Processing path constraint: '+c.label)
+        logging.debug('Processing path constraint: '+str(c.name))
         order = 0
-        cq = [sympify(c.expr)]
-        dxdt = [sympify(state.process_eqn) for state in problem.states()]
+        cq = [c.expr]
+        dxdt = [state.eom for state in states]
 
         # Zeroth order constraints have no 'xi' state
         xi_vars = []
         h = []
         while True:
             control_found = False
-
-            for u in problem.controls():
-                if u.sym in cq[-1].subs(quantity_subs).atoms():
+            for u in controls:
+                if u in cq[-1].subs(quantity_vars).atoms():
                     logging.info('Constraint is of order '+str(order))
                     control_found = True
                     break
             if control_found:
                 break
 
-            dcdx = [self.derivative(cq[-1], state.sym, self.quantity_vars) for state in problem.states()]
+            dcdx = [derivative_fn(cq[-1], state) for state in states]
 
             # Chain rule (Assume there is no explciit time-dependence) to find dcdt
             cq.append(sum(d1*d2 for d1,d2 in zip(dcdx, dxdt)))
             order = order + 1
 
             # Create the auxiliary state variables
-            xi_vars.append(Symbol('xi'+str(ind+1)+str(order)))
+            xi_vars.append(sym.Symbol('xi'+str(ind+1)+str(order)))
 
         # Create the smoothing control variable
-        xi_vars.append(Symbol('ue'+str(ind+1)))
+        xi_vars.append(sym.Symbol('ue'+str(ind+1)))
 
         # TODO: Fix constraint object to accept two limits
-        c_limit = sympify(c.limit)
+        c_limit = sym.sympify(c.bound)
         if c_limit.is_Number:
             # TODO: Allow continuation on constraints
             # Define new hidden constant
-            c_limit = sympify('_'+c.label)
-            print(c.limit)
-            problem.constant(str(c_limit),float(c.limit),c.unit)
+            c_limit = sympify('_'+str(c.name))
+            ocp.constant(str(c_limit),float(c.bound),c.unit)
             logging.debug('Added constant '+str(c_limit))
 
         if c.direction == '>':
@@ -85,21 +102,22 @@ def process_path_constraints(self, workspace):
         else:
             raise ValueError('Invalid direction specified for constraint')
 
-        psi = self.get_satfn(xi_vars[0], ubound=c.ubound, lbound=c.lbound, slopeAtZero=50)
-        psi_vars = [(Symbol('psi'+str(ind+1)), psi)]
+        psi = get_satfn(xi_vars[0], ubound=c.ubound, lbound=c.lbound, slopeAtZero=50)
+        psi_vars = [(sym.Symbol('psi'+str(ind+1)), psi)]
 
         # Add to quantity list
-        self.quantity_vars[Symbol('psi'+str(ind+1))] = psi
-        self.quantity_list.append({'name':('psi'+str(ind+1)), 'expr':str(psi)})
+        quantity_vars[sym.Symbol('psi'+str(ind+1))] = psi
+        quantity_list.append({'name':('psi'+str(ind+1)), 'expr':str(psi)})
 
         # m-th order constraint needs up to m-th derivative of psi to be defined
         psi_i = psi
+
         for i in range(order):
-            psi_i = diff(psi_i, xi_vars[0])
+            psi_i = derivative_fn(psi_i, xi_vars[0])
             # psi_vars.append((Symbol('psi'+str(ind+1)+str(i+1)+'('+str(xi_vars[0])+')'), psi_i))
-            psi_vars.append((Symbol('psi'+str(ind+1)+str(i+1)), psi_i))
-            self.quantity_vars[Symbol('psi'+str(ind+1)+str(i+1))] =  psi_i
-            self.quantity_list.append({'name':('psi'+str(ind+1)+str(i+1)), 'expr':str(psi_i)})
+            psi_vars.append((sym.Symbol('psi'+str(ind+1)+str(i+1)), psi_i))
+            quantity_vars['psi'+str(ind+1)+str(i+1)] =  psi_i
+            quantity_list.append({'name':('psi'+str(ind+1)+str(i+1)), 'expr':str(psi_i)})
 
         # psi_vars = psi_vars + []
         psi_var_sub = [(v,k) for k,v in psi_vars]
@@ -117,63 +135,169 @@ def process_path_constraints(self, workspace):
         h = [psi_vars[0][1]]
         for i in range(order):
             # Add 'xi' state
-            problem.state(str(xi_vars[i]), str(xi_vars[i+1]),'('+c.unit+')/s^('+str(i)+')')
+            states.append(SymVar({'name':str(xi_vars[i]), 'eom':str(xi_vars[i+1]), 'unit':c.unit/(time_unit^i)}))
             # Constraint all cq at initial point (forms constraints for xi_ij)
-            problem.constraints().initial(str(cq[i] - h[i]),'('+c.unit+')/s^('+str(i)+')')
+            # ocp.constraints().initial(str(cq[i] - h[i]),'('+c.unit+')/s^('+str(i)+')')
+            constraints['initial'].append(SymVar({'expr':str(cq[i] - h[i]), 'unit':c.unit/(time_unit^i)}, sym_key='expr'))
             # Add to initial guess vector
-            problem.guess.start.append(c_vals[i])
+            # guess.start.append(c_vals[i]) # FIXME
 
-            dhdxi = [diff(h[i], xi_v) for xi_v in xi_vars[:-1]]
+            dhdxi = [derivative_fn(h[i], xi_v) for xi_v in xi_vars[:-1]]
             dhdt  = sum(d1*d2 for d1,d2 in zip(dhdxi,xi_vars[1:])) # xi11dot = xi12 etc.
             dhdt = dhdt.subs(psi_var_sub)
             h.append(dhdt)
 
         # Add the smoothing control with the right unit
-        ue_unit = sympify('('+c.unit+')/(s^('+str(order)+'))')
-        problem.control(str(xi_vars[-1]), str(ue_unit))
+        ue_unit = c.unit/(time_unit**order)
+        controls.append(SymVar({'name':str(xi_vars[-1]), 'unit':str(ue_unit)}))
+
         logging.debug('Adding control '+str(xi_vars[-1])+' with unit '+str(ue_unit))
 
         # Add equality constraint
         cqi_unit = ue_unit*time_unit
-        problem.constraints().equality(str(cq[-1] - h[-1]),str(cqi_unit))
+        # ocp.constraints().equality(str(cq[-1] - h[-1]),str(cqi_unit))
+
+        eq.append(SymVar({'type':'equality', 'expr':str(cq[-1] - h[-1]), 'unit':str(cqi_unit), 'name':'_eq_'+str(ind)}))
 
         # Add smoothing factor
-        eps_const = Symbol('eps_'+c.label)
+        eps_const = sympify('eps_'+str(c.name))
         eps_unit = (path_cost_unit/ue_unit**2)/time_unit #Unit of integrand
-        problem.constant(str(eps_const), 1e-2, str(eps_unit))
+        # ocp.constant(str(eps_const), 1e-2, str(eps_unit))
+        constants.append(SymVar({'name':eps_const, 'value': 1e-2, 'unit':str(eps_unit)}))
         logging.debug('Adding smoothing factor '+str(eps_const)+' with unit '+str(eps_unit))
 
         # Append new control to path cost
         path_cost_expr = path_cost_expr + eps_const*xi_vars[-1]**2
 
+    constraints['equality'] = eq
+
     logging.debug('Updated path cost is: '+str(path_cost_expr))
-    problem.cost['path'].expr = str(path_cost_expr)
 
-    u_constraints = problem.constraints().get('control')
+    derivative_fn = ft.partial(total_derivative, dependent_vars=quantity_vars)
+    jacobian_fn = ft.partial(jacobian, derivative_fn=derivative_fn)
 
-    for (ind,c) in enumerate(u_constraints):
-        w_i = sympify('uw'+str(ind+1))
-        psi = self.get_satfn(w_i, ubound=sympify(c.ubound), lbound = sympify(c.lbound))
+    s_list = []
 
-        # Add the smoothing control
-        problem.control(str(w_i), c.unit)
+    yield states
+    yield controls
+    yield constants
+    yield constraints
+    yield s_list
+    yield derivative_fn
+    yield jacobian_fn
+    # ocp.path_cost(str(path_cost_expr), )
 
-        # Add equality constraint
-        csym = sympify(c.expr)
-        problem.constraints().equality(str(csym - psi),c.unit)
+    # u_constraints = ocp.constraints().get('control')
+    #
+    # for (ind,c) in enumerate(u_constraints):
+    #     w_i = sympify('uw'+str(ind+1))
+    #     psi = self.get_satfn(w_i, ubound=sympify(c.ubound), lbound = sympify(c.lbound))
+    #
+    #     # Add the smoothing control
+    #     problem.control(str(w_i), c.unit)
+    #
+    #     # Add equality constraint
+    #     csym = sympify(c.expr)
+    #     problem.constraints().equality(str(csym - psi),c.unit)
+    #
+    #     uw_unit = symipfy2(c.unit)
+    #     eps_const = Symbol('eps_'+str(ind+1))
+    #     eps_unit = (path_cost_unit/uw_unit**2)/time_unit #Unit of integrand
+    #     problem.constant(str(eps_const), 1, str(eps_unit))
+    return ocp
 
-        uw_unit = symipfy2(c.unit)
-        eps_const = Symbol('eps_'+str(ind+1))
-        eps_unit = (path_cost_unit/uw_unit**2)/time_unit #Unit of integrand
-        problem.constant(str(eps_const), 1, str(eps_unit))
+def make_ctrl_dae(states, costates, controls, constraints, dhdu, derivative_fn):
+    equality_constraints = constraints.get('equality', [])
+    if len(equality_constraints) > 0:
+        mu_vars = [sympify('mu'+str(i+1)) for i in range(len(equality_constraints))]
+        mu_lhs = [sympify(c.expr) for c in equality_constraints]
+    else:
+        mu_vars = mu_lhs = []
 
+    g = dhdu + mu_lhs
+    X = [state for state in states] + [costate for costate in costates]
+    U = [c for c in controls] + mu_vars
+
+    xdot = sym.Matrix([sympify(state.eom) for state in states] + [sympify(lam.eom) for lam in costates])
+    # Compute Jacobian
+    dgdX = sym.Matrix([[derivative_fn(g_i, x_i) for x_i in X] for g_i in g])
+    dgdU = sym.Matrix([[derivative_fn(g_i, u_i) for u_i in U] for g_i in g])
+
+    udot = dgdU.LUsolve(-dgdX*xdot); # dgdU * udot + dgdX * xdot = 0
+
+    dae_states = U
+    dae_equations = list(udot)
+    dae_bc = g
+
+    yield mu_vars
+    yield mu_lhs
+    yield dae_states
+    yield dae_equations
+    yield dae_bc
+def generate_problem_data(workspace):
+    """Generates the `problem_data` dictionary used for code generation."""
+
+    tf_var = sympify('tf') #TODO: Change to independent var?
+
+    problem_data = {
+    'problem_name': workspace['problem_name'],
+    'aux_list': [
+            {
+            'type' : 'const',
+            'vars': [str(k) for k in workspace['constants']]
+            }
+     ],
+     'state_list':
+         [str(x) for x in it.chain(workspace['states'], workspace['costates'])]
+         + ['tf']
+     ,
+     'parameter_list': [str(p) for p in workspace['parameters']],
+     'x_deriv_list': [str(tf_var*state.eom) for state in workspace['states']],
+     'lam_deriv_list':[str(tf_var*costate.eom) for costate in workspace['costates']],
+     'deriv_list':
+         [str(tf_var*state.eom) for state in workspace['states']] +
+         [str(tf_var*costate.eom) for costate in workspace['costates']] +
+         [0]   # TODO: Hardcoded 'tf'
+     ,
+     'states': workspace['states'],
+     'costates': workspace['costates'],
+     'constants': workspace['constants'],
+     'parameters': workspace['parameters'],
+     'controls': workspace['controls'],
+     'mu_vars': workspace['mu_vars'],
+     'quantity_vars': workspace['quantity_vars'],
+     'dae_var_list':
+         [str(dae_state) for dae_state in workspace['dae_states']],
+     'dae_eom_list':
+         ['(tf)*('+str(dae_eom)+')' for dae_eom in workspace['dae_equations']],
+     'dae_var_num': len(workspace['dae_states']),
+
+     'costate_eoms': [ {'eom':[str(_.eom*tf_var) for _ in workspace['costates']], 'arctype':0} ],
+     'ham': workspace['ham'],
+
+     's_list': [],
+     'bc_list': [],
+     'num_states': 2*len(workspace['states']) + 1,
+     'num_params': len(workspace['parameters']),
+     'dHdu': [str(_) for _ in it.chain(workspace['dhdu'], workspace['mu_lhs'])],
+     'bc_initial': [str(_) for _ in it.chain(workspace['bc_initial'], workspace['dae_bc'])],
+     'bc_terminal': [str(_) for _ in workspace['bc_terminal']],
+     'control_options': [],
+     'control_list': [str(u) for u in workspace['controls']+workspace['mu_vars']],
+     'num_controls': len(workspace['controls'])+len(workspace['mu_vars']),
+     'ham_expr': str(workspace['ham']),
+     'quantity_list': workspace['quantity_list'],
+    }
+
+    return problem_data
 # Implement workflow using simplepipe and functions defined above
 ICRM = sp.Workflow([
-    sp.Task(process_quantities,
-            inputs=('quantities'),
-            outputs=('quantity_vars', 'quantity_list', 'derivative_fn')),
+    sp.Task(init_workspace, inputs=('problem',), outputs='*'),
+    sp.Task(process_quantities, inputs=('quantities'),
+            outputs=('quantity_vars', 'quantity_list', 'derivative_fn', 'jacobian_fn')),
+    sp.Task(process_path_constraints, inputs='*',
+            outputs=('states', 'controls', 'constants', 'constraints', 's_list', 'derivative_fn', 'jacobian_fn')),
 
-    sp.Task(process_path_constraints, inputs='*', outputs=('oo')),
     sp.Task(ft.partial(make_augmented_cost, location='initial'),
             inputs=('initial_cost', 'constraints'),
             outputs=('aug_initial_cost')),
@@ -187,8 +311,11 @@ ICRM = sp.Workflow([
     sp.Task(ft.partial(make_aug_params, location='terminal'),
             inputs=('constraints'),
             outputs=('terminal_lm_params')),
-    sp.Task(make_hamiltonian_and_costates,
-            inputs=('states', 'path_cost', 'derivative_fn'),
+    sp.Task(make_costate_names,
+            inputs=('states'),
+            outputs=('costate_names')),
+    sp.Task(make_hamiltonian_and_costate_rates,
+            inputs=('states', 'costate_names', 'path_cost', 'derivative_fn'),
             outputs=('ham', 'costates')),
     sp.Task(ft.partial(make_boundary_conditions, location='initial'),
             inputs=('constraints', 'states', 'costates', 'aug_initial_cost', 'derivative_fn'),
@@ -200,14 +327,23 @@ ICRM = sp.Workflow([
     sp.Task(make_dhdu,
             inputs=('ham', 'controls', 'derivative_fn'),
             outputs=('dhdu')),
-    sp.Task(make_control_law,
-            inputs=('dhdu','controls'),
-            outputs=('control_law')),
-
+    sp.Task(make_ctrl_dae,
+            inputs=('states', 'costates', 'controls', 'constraints', 'dhdu', 'derivative_fn'),
+            outputs=('mu_vars', 'mu_lhs', 'dae_states', 'dae_equations', 'dae_bc')),
+    sp.Task(make_parameters, inputs=['initial_lm_params', 'terminal_lm_params', 's_list'],
+        outputs='parameters'),
+    # sp.Task(make_dhdu,
+    #         inputs=('ham', 'controls', 'derivative_fn'),
+    #         outputs=('dhdu')),
+    #
+    # sp.Task(make_control_law,
+    #         inputs=('dhdu','controls'),
+    #         outputs=('control_law')),
+    #
     sp.Task(generate_problem_data,
             inputs='*',
             outputs=('problem_data')),
-], description='Traditional optimal control workflow')
+], description='ICRM workflow')
 
 
 # Call doctest if the script is run directly
--- examples/icrm/boundedBrachistochrone/boundedBrachistochrone.py
+++ examples/icrm/boundedBrachistochrone/boundedBrachistochrone.py
@@ -1,81 +1,153 @@
 """Brachistochrone example."""
 
-import numpy as np
+ocp = beluga.OCP('brachisto')
 
-import beluga.Beluga as Beluga
-import beluga.bvpsol as bvpsol
-import beluga.bvpsol.algorithms as algorithms
-import beluga.optim.Problem
-from beluga.optim.problem import *
-from beluga.continuation import *
+# Define independent variables
+ocp.independent('t', 's')
 
-def get_problem():
-    """Brachistochrone example."""
+# Define equations of motion
+ocp.state('x', 'v*cos(theta)', 'm')   \
+   .state('y', '-v*sin(theta)','m')   \
+   .state('v', 'g*sin(theta)','m/s')
 
-    # Rename this and/or move to optim package?
-    problem = beluga.optim.Problem('boundedBrachistochrone')
+# Define controls
+ocp.control('theta','rad')
 
-    # Define independent variables
-    problem.independent('t', 's')
+# Define constants
+ocp.constant('g',9.81,'m/s^2')
+ocp.constant('xlim',1.0,'m')
 
-    # Define equations of motion
-    problem.state('x', 'v*cos(theta)','m')   \
-           .state('y', '-v*sin(theta)','m')   \
-           .state('v', 'g*sin(theta)','m/s')
-    # Define controls
-    problem.control('theta','rad')
+# Define costs
+ocp.path_cost('1','s')
 
-    # Define costs
-    problem.cost['path'] = Expression('1','s')
+# Define constraints
+ocp.constraints() \
+    .initial('x-x_0','m')    \
+    .initial('y-y_0','m')    \
+    .initial('v-v_0','m/s')  \
+    .terminal('x-x_f','m')   \
+    .terminal('y-y_f','m') \
+    .path('constraint1','y + x','>','xlim','m')
+    # .path('constraint2','y + 0.75*x','>',-2,'m')  #\
 
 
-    # Define constraints
-    problem.constraints('default',0) \
-                        .initial('x-x_0','m')    \
-                        .initial('y-y_0','m')    \
-                        .initial('v-v_0','m/s')  \
-                        .terminal('x-x_f','m')   \
-                        .terminal('y-y_f','m')   \
-                        .path('constraint1','y + 1.5*x','>',-0.5,'m')\
-                        .path('constraint2','y + 0.75*x','>',-2,'m')  #\
-                        # .path('constraint1','y + x','>',-1,'m')  #\
-                        # .path('constraint2','y - 0.5*x','>',-2,'m')
-                        # y + x > h0 -- above the line y= -x + h0
+ocp.scale(m='y', s='y/v', kg=1, rad=1)
+# ocp.scale(m=1, s=1, kg=1, rad=1)
 
-    # Define constants
-    problem.constant('g','9.81','m/s^2')
 
-    # Scaling poptions
-    problem.scale.unit('m','x')     \
-                   .unit('s','x/v')\
-                   .unit('kg',1)   \
-                   .unit('rad',1)
+bvp_solver = beluga.bvp_algorithm('MultipleShooting',
+                    derivative_method='fd',
+                    tolerance=1e-4,
+                    max_iterations=50,
+                    verbose = True,
+                    max_error=50
+)
 
-    # problem.bvp_solver = algorithms.MultipleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=1000, verbose = True, cached=False, number_arcs=2)
-    problem.bvp_solver = algorithms.SingleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=1000, verbose = True, cached=False)
-    # problem.bvp_solver = algorithms.BroydenShooting(tolerance=1e-4, max_iterations=1000)
+guess_maker = beluga.guess_generator('auto',
+                start=[0,0,1],          # Starting values for states in order
+                direction='forward',
+                costate_guess = -0.1
+)
 
-    # Can be array or function handle
-    # TODO: implement an "initial guess" class subclassing Solution
-    # problem.guess = bvpsol.bvpinit(np.linspace(0,1,2), [0,0,1,-0.1,-0.1,-0.1,0.1])
-    # problem.guess.parameters = np.array([0.1,0.1,0.1,0.1,0.1])
-    problem.guess.setup('auto',
-                    start=[0,0,1],          # Starting values for states in order
-                    direction='forward',
-                    costate_guess = 0.1
-                    )
+continuation_steps = beluga.init_continuation()
 
-    # Figure out nicer way of representing this. Done?
-    problem.steps.add_step().num_cases(41) \
-                    .terminal('x', 5) \
-                    .terminal('y',-5)
+continuation_steps.add_step('bisection') \
+                .num_cases(11) \
+                .terminal('x', 10) \
+                .terminal('y',-10)
+#
+# continuation_steps.add_step('activate_constraint', name='constraint1')
+#
+# continuation_steps.add_step('bisection') \
+#                 .num_cases(11) \
+#                 .constraint('constraint1', 0.0, index=1)
 
-    # TODO: Automate addition of epsilon continuation
-    problem.steps.add_step('bisection').num_cases(41,spacing='log') \
-                     .const('eps_constraint1', 1e-6) #\
-                    #  .const('eps_constraint2', 1e-6)
 
-    return problem
+beluga.solve(ocp,
+             method='icrm',
+             bvp_algorithm=bvp_solver,
+             steps=continuation_steps,
+             guess_generator=guess_maker)
 
-if __name__ == '__main__':
-    Beluga.run(get_problem())
+
+# """Brachistochrone example."""
+#
+# import numpy as np
+#
+# import beluga.Beluga as Beluga
+# import beluga.bvpsol as bvpsol
+# import beluga.bvpsol.algorithms as algorithms
+# import beluga.optim.Problem
+# from beluga.optim.problem import *
+# from beluga.continuation import *
+#
+# def get_problem():
+#     """Brachistochrone example."""
+#
+#     # Rename this and/or move to optim package?
+#     problem = beluga.optim.Problem('boundedBrachistochrone')
+#
+#     # Define independent variables
+#     problem.independent('t', 's')
+#
+#     # Define equations of motion
+#     problem.state('x', 'v*cos(theta)','m')   \
+#            .state('y', '-v*sin(theta)','m')   \
+#            .state('v', 'g*sin(theta)','m/s')
+#     # Define controls
+#     problem.control('theta','rad')
+#
+#     # Define costs
+#     problem.cost['path'] = Expression('1','s')
+#
+#
+#     # Define constraints
+#     problem.constraints('default',0) \
+#                         .initial('x-x_0','m')    \
+#                         .initial('y-y_0','m')    \
+#                         .initial('v-v_0','m/s')  \
+#                         .terminal('x-x_f','m')   \
+#                         .terminal('y-y_f','m')   \
+#                         .path('constraint1','y + 1.5*x','>',-0.5,'m')\
+#                         .path('constraint2','y + 0.75*x','>',-2,'m')  #\
+#                         # .path('constraint1','y + x','>',-1,'m')  #\
+#                         # .path('constraint2','y - 0.5*x','>',-2,'m')
+#                         # y + x > h0 -- above the line y= -x + h0
+#
+#     # Define constants
+#     problem.constant('g','9.81','m/s^2')
+#
+#     # Scaling poptions
+#     problem.scale.unit('m','x')     \
+#                    .unit('s','x/v')\
+#                    .unit('kg',1)   \
+#                    .unit('rad',1)
+#
+#     # problem.bvp_solver = algorithms.MultipleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=1000, verbose = True, cached=False, number_arcs=2)
+#     problem.bvp_solver = algorithms.SingleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=1000, verbose = True, cached=False)
+#     # problem.bvp_solver = algorithms.BroydenShooting(tolerance=1e-4, max_iterations=1000)
+#
+#     # Can be array or function handle
+#     # TODO: implement an "initial guess" class subclassing Solution
+#     # problem.guess = bvpsol.bvpinit(np.linspace(0,1,2), [0,0,1,-0.1,-0.1,-0.1,0.1])
+#     # problem.guess.parameters = np.array([0.1,0.1,0.1,0.1,0.1])
+#     problem.guess.setup('auto',
+#                     start=[0,0,1],          # Starting values for states in order
+#                     direction='forward',
+#                     costate_guess = 0.1
+#                     )
+#
+#     # Figure out nicer way of representing this. Done?
+#     problem.steps.add_step().num_cases(41) \
+#                     .terminal('x', 5) \
+#                     .terminal('y',-5)
+#
+#     # TODO: Automate addition of epsilon continuation
+#     problem.steps.add_step('bisection').num_cases(41,spacing='log') \
+#                      .const('eps_constraint1', 1e-6) #\
+#                     #  .const('eps_constraint2', 1e-6)
+#
+#     return problem
+#
+# if __name__ == '__main__':
+#     Beluga.run(get_problem())
