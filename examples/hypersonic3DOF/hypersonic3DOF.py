af29a719b388b15b3a5b177e6c95b9e0e6dfc0ec
Thomas Antony <tantony@purdue.edu>
2017-10-25 10:26:08 -0500

    Before switching to master

--- examples/hypersonic3DOF/hypersonic3DOF.py
+++ examples/hypersonic3DOF/hypersonic3DOF.py
@@ -1,103 +1,99 @@
-import numpy as np
+from math import pi
+# Figure out way to implement caching automatically
+# @functools.lru_cache(maxsize=None)
+# def CLfunction(alfa):
+#     return 1.5658*alfa
+#
+# # @functools.lru_cache(maxsize=None)
+# def CDfunction(alfa):
+#     return 1.6537*alfa**2 + 0.0612
+#
+# Rename this and/or move to optim package?
+ocp = beluga.OCP('hypersonic3DOF')
 
-import beluga.Beluga as Beluga
-import beluga.bvpsol as bvpsol
-import beluga.bvpsol.algorithms as algorithms
-import beluga.optim.Problem
-from beluga.optim.problem import *
-from beluga.continuation import *
-from math import *
+# Define independent variables
+ocp.independent('t', 's')
 
-"""Hypersonic 3DOF dynamics example."""
-def get_problem():
-    # Figure out way to implement caching automatically
-    # @functools.lru_cache(maxsize=None)
-    def CLfunction(alfa):
-        return 1.5658*alfa
+rho = 'rho0*exp(-h/H)'
+Cl  = '(1.5658*alfa + -0.0000)'
+Cd  = '(1.6537*alfa^2 + 0.0612)'
+# Cl = 'CLfunction(alfa)'
+# Cd = 'CDfunction(alfa)'
+D   = '(0.5*'+rho+'*v^2*'+Cd+'*Aref)'
+L   = '(0.5*'+rho+'*v^2*'+Cl+'*Aref)'
+r   = '(re+h)'
 
-    # @functools.lru_cache(maxsize=None)
-    def CDfunction(alfa):
-        return 1.6537*alfa**2 + 0.0612
+# Define equations of motion
+ocp.state('h','v*sin(gam)','m')                                     \
+       .state('theta','v*cos(gam)*cos(psi)/('+r+'*cos(phi))','rad')    \
+       .state('phi','v*cos(gam)*sin(psi)/'+r,'rad')                    \
+       .state('v','-'+D+'/mass - mu*sin(gam)/'+r+'^2','m/s')            \
+       .state('gam',L+'*cos(bank)/(mass*v) - mu/(v*'+r+'^2)*cos(gam) + v/'+r+'*cos(gam)','rad')                                         \
+       .state('psi',L+'*sin(bank)/(mass*cos(gam)*v) - v/'+r+'*cos(gam)*cos(psi)*tan(phi)','rad')
 
-    # Rename this and/or move to optim package?
-    problem = beluga.optim.Problem('hypersonci3DOF')
+# Define controls
+ocp.control('bank','rad') \
+       .control('alfa','rad')
 
-    # Define independent variables
-    problem.independent('t', 's')
+# Define costs
+ocp.terminal_cost('-v^2','m^2/s^2')
 
-    rho = 'rho0*exp(-h/H)'
-    Cl  = '(1.5658*alfa + -0.0000)'
-    Cd  = '(1.6537*alfa^2 + 0.0612)'
-    # Cl = 'CLfunction(alfa)'
-    # Cd = 'CDfunction(alfa)'
-    D   = '(0.5*'+rho+'*v^2*'+Cd+'*Aref)'
-    L   = '(0.5*'+rho+'*v^2*'+Cl+'*Aref)'
-    r   = '(re+h)'
+# Define constraints
+ocp.constraints().initial('h-h_0','m')              \
+                 .initial('theta-theta_0','rad')     \
+                 .initial('phi-phi_0','rad')         \
+                 .initial('v-v_0','m/s')             \
+                 .terminal('h-h_f','m')              \
+                 .terminal('theta-theta_f','rad')    \
+                 .terminal('phi-phi_f','rad') \
+                 .initial('psi-psi_0','rad')
 
-    # Define equations of motion
-    problem.state('h','v*sin(gam)','m')                                     \
-           .state('theta','v*cos(gam)*cos(psi)/('+r+'*cos(phi))','rad')    \
-           .state('phi','v*cos(gam)*sin(psi)/'+r,'rad')                    \
-           .state('v','-'+D+'/mass - mu*sin(gam)/'+r+'^2','m/s')            \
-           .state('gam',L+'*cos(bank)/(mass*v) - mu/(v*'+r+'^2)*cos(gam) + v/'+r+'*cos(gam)','rad')                                         \
-           .state('psi',L+'*sin(bank)/(mass*cos(gam)*v) - v/'+r+'*cos(gam)*cos(psi)*tan(phi)','rad')
 
-    # Define controls
-    problem.control('bank','rad') \
-           .control('alfa','rad')
+# Define constants
+ocp.constant('mu',3.986e5*1e9,'m^3/s^2') # Gravitational parameter, m^3/s^2
+ocp.constant('rho0',1.2,'kg/m^3') # Sea-level atmospheric density, kg/m^3
+ocp.constant('H',7500,'m') # Scale height for atmosphere of Earth, m
+ocp.constant('mass',750/2.2046226,'kg') # Mass of vehicle, kg
+ocp.constant('re',6378000,'m') # Radius of planet, m
+ocp.constant('Aref',pi*(24*.0254/2)**2,'m^2') # Reference area of vehicle, m^2
+ocp.constant('rn',1/12*0.3048,'m') # Nose radius, m
 
-    # Define costs
-    problem.cost['terminal'] = Expression('-v^2','m^2/s^2')
 
-    # Define constraints
-    problem.constraints().initial('h-h_0','m')              \
-                         .initial('theta-theta_0','rad')     \
-                         .initial('phi-phi_0','rad')         \
-                         .initial('v-v_0','m/s')             \
-                         .terminal('h-h_f','m')              \
-                         .terminal('theta-theta_f','rad')    \
-                         .terminal('phi-phi_f','rad')
+ocp.scale(m='h', s='h/v', kg='mass', rad=1)
 
-    # Define constants
-    problem.constant('mu',3.986e5*1e9,'m^3/s^2') # Gravitational parameter, m^3/s^2
-    problem.constant('rho0',1.2,'kg/m^3') # Sea-level atmospheric density, kg/m^3
-    problem.constant('H',7500,'m') # Scale height for atmosphere of Earth, m
-    problem.constant('mass',750/2.2046226,'kg') # Mass of vehicle, kg
-    problem.constant('re',6378000,'m') # Radius of planet, m
-    problem.constant('Aref',pi*(24*.0254/2)**2,'m^2') # Reference area of vehicle, m^2
-    problem.constant('rn',1/12*0.3048,'m') # Nose radius, m
+bvp_solver = beluga.bvp_algorithm('MultipleShooting',
+                        derivative_method='fd',
+                        tolerance=1e-4,
+                        max_iterations=100,
+                        verbose = True,
+                        max_error=100,
+             )
 
-    problem.scale.unit('m','h')     \
-                   .unit('s','h/v')\
-                   .unit('kg','mass')   \
-                   .unit('rad',1)
+guess_maker = beluga.guess_generator('auto',
+                start=[80000,0,0,5000,-(90+5)*pi/180, 0],
+                direction='forward',
+                costate_guess = -0.01,
+                control_guess=[0.0,0.0],
+                time_integrate=0.01,
+)
 
-    # Define quantity (not implemented at present)
-    # Is this actually an Expression rather than a Value?
-    # problem.quantity = [Value('tanAng','tan(theta)')]
+continuation_steps = beluga.init_continuation()
 
-    #problem.bvp_solver = algorithms.SingleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=1000, verbose = True, cached = False)
-    problem.bvp_solver = algorithms.MultipleShooting(derivative_method='fd',tolerance=1e-4, max_iterations=4000, verbose = True, cached = False, number_arcs=4)
+continuation_steps.add_step().num_cases(11)           \
+                        .terminal('h',0)
+#
+# continuation_steps.add_step().num_cases(11)          \
+#                         .initial('h',80000)
 
-    problem.guess.setup('auto',start=[80000,0,0,5000,(-90+10)*pi/180,0])
+continuation_steps.add_step().num_cases(41)          \
+                        .terminal('theta',5*pi/180) \
+                        .terminal('phi',2*pi/180)
 
-    problem.steps.add_step().num_cases(11)           \
-                            .terminal('h',0)
-    #
-    # problem.steps.add_step().num_cases(11)          \
-    #                         .initial('h',80000)
-
-    problem.steps.add_step().num_cases(41)          \
-                            .terminal('theta',5*pi/180) \
-                            .terminal('phi',2*pi/180)
-
-    # problem.steps.add_step().num_cases(10)              \
-    #                         .terminal('theta',5*pi/180) \
-    #                         .terminal('phi',5*pi/180)
-
-    return problem
-
-if __name__ == '__main__':
-    problem = get_problem()
-    # Default solver is a forward-difference Single Shooting solver with 1e-4 tolerance
-    Beluga.run(problem)
+# continuation_steps.add_step().num_cases(10)              \
+#                         .terminal('theta',5*pi/180) \
+#                         .terminal('phi',5*pi/180)
+beluga.solve(ocp,
+             method='icrm',
+             bvp_algorithm=bvp_solver,
+             steps=continuation_steps,
+             guess_generator=guess_maker)
