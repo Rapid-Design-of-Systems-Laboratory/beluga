27cfdb840909a5585c77912ce225b35114ef98b0
Thomas Antony <tantony@purdue.edu>
2017-10-28 17:58:30 -0500

    Twoside solution.

--- examples/test/plot.py
+++ examples/test/plot.py
@@ -5,8 +5,8 @@ plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1, renderer='matpl
 # plots.add_plot().line('xbar*V*tfreal','ybar*V*tfreal')                    \
 #                 .xlabel('x(t)').ylabel('y(t)')      \
 #                 .title('Trajectory')
-plots.add_plot().line('xbar','ybar')                    \
-                .line('xbar2','ybar2')                    \
+plots.add_plot().line('xbar','ybar',label='traj1')                    \
+                .line('xbar2','ybar2',label='traj2')                    \
                 .xlabel('x(t)').ylabel('y(t)')      \
                 .title('Trajectory') \
                 .postprocess(lambda a,b,c: plt.axis('equal'))
@@ -21,8 +21,9 @@ plots.add_plot().line('t','abar*V/tfreal')                    \
                 .xlabel('t (s)').ylabel('a (rad/s)')      \
                 .title('Control history')
 
-plots.add_plot().line('t','sqrt((xbar - xbar2)**2 + (ybar - ybar2)**2)*(V*tfreal)')                    \
-                .xlabel('t').ylabel('Separation (m)')      \
-                .title('Separation')
+# plots.add_plot().line('t','sqrt((xbar - xbar2)**2 + (ybar - ybar2)**2)*(V*tfreal)')                    \
+#                 .xlabel('t').ylabel('Separation (m)')      \
+#                 .title('Separation')
+
 
 plots.render()
