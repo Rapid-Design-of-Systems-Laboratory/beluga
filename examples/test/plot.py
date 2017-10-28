502b92dab87f0f0dd506b125e6783e9259c2c990
Thomas Antony <tantony@purdue.edu>
2017-10-28 15:47:52 -0500

    Solved two vehicle problem.

--- examples/test/plot.py
+++ examples/test/plot.py
@@ -6,14 +6,17 @@ plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1, renderer='matpl
 #                 .xlabel('x(t)').ylabel('y(t)')      \
 #                 .title('Trajectory')
 plots.add_plot().line('xbar','ybar')                    \
+                .line('xbar2','ybar2')                    \
                 .xlabel('x(t)').ylabel('y(t)')      \
                 .title('Trajectory')
 
 plots.add_plot().line('t','psi*180/3.14')                    \
+                .line('t','psi2*180/3.14')                    \
                 .xlabel('t (s)').ylabel('psi (deg)')      \
                 .title('Heading')
 
 plots.add_plot().line('t','abar*V/tfreal')                    \
+                .line('t','abar2*V/tfreal')                    \
                 .xlabel('t (s)').ylabel('a (rad/s)')      \
                 .title('Control history')
 plots.render()
