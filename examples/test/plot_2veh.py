2287ffa4cb2fe9523c5bf519da35c1383b69a2c3
Thomas Antony <tantony@purdue.edu>
2017-10-28 18:12:02 -0500

    Added free-time two vehicle example.

--- examples/test/plot_2veh.py
+++ examples/test/plot_2veh.py
@@ -6,15 +6,18 @@ plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1, renderer='matpl
 #                 .xlabel('x(t)').ylabel('y(t)')      \
 #                 .title('Trajectory')
 plots.add_plot().line('xbar','ybar',label='traj1')                    \
+                .line('xbar2','ybar2',label='traj2')                    \
                 .xlabel('x(t)').ylabel('y(t)')      \
                 .title('Trajectory') \
                 .postprocess(lambda a,b,c: plt.axis('equal'))
 
 plots.add_plot().line('t','psi*180/3.14')                    \
+                .line('t','psi2*180/3.14')                    \
                 .xlabel('t (s)').ylabel('psi (deg)')      \
                 .title('Heading')
 
 plots.add_plot().line('t','abar*V/tfreal')                    \
+                .line('t','abar2*V/tfreal')                    \
                 .xlabel('t (s)').ylabel('a (rad/s)')      \
                 .title('Control history')
 
