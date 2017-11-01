b175f8d421721ae8159e41169a69b814c62144e0
Thomas Antony <tantony@purdue.edu>
2017-11-01 01:28:06 -0500

    Bounded brachistochrone working settings for QCPI.

--- examples/qcpi/boundedBrachistochrone/plot.py
+++ examples/qcpi/boundedBrachistochrone/plot.py
@@ -4,8 +4,8 @@ from beluga.visualization.datasources import Dill
 unc_ds = Dill('../../mpbvp/boundedBrachistochrone/data.dill')
 plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1, renderer='matplotlib')
 
-plots.add_plot().line('x','y',label='Solution', sol=-1, step=-1)               \
-                .line('x','y',label='Unconstrained Solution', datasource=unc_ds, step=0, sol=-1)\
+plots.add_plot().line('x','y',label='Solution', sol=-1, step=-1,style={'lw':2.0})               \
+                .line('x','y',label='Unconstrained Solution', datasource=unc_ds, step=0, sol=-1,style={'lw':2.0})\
                 .line('x','xlim-x',label='Constraint1',step=-1,sol=-1) \
                 .xlabel('x(t)').ylabel('y(t)')      \
                 .title('Trajectory')
