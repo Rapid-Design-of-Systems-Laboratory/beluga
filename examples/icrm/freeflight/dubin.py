3f4183bfd5e6bf3d7617138f98499156ab479ecc
Thomas Antony <tantony@purdue.edu>
2017-11-02 19:15:22 -0500

    Some local changes.

--- examples/icrm/freeflight/dubin.py
+++ examples/icrm/freeflight/dubin.py
@@ -2,32 +2,34 @@
 import beluga
 from math import *
 
-ocp = beluga.OCP('dubins')
+ocp = beluga.OCP('dubin')
 
 # Define independent variables
 ocp.independent('t', 's')
 
 # Define equations of motion
-ocp.state('x','V*cos(theta)','m')   \
-   .state('y','V*sin(theta)','m')  \
-   .state('theta','-V/L*delta','rad')
-ocp.state('x1','v1*cos(theta1)','m')   \
-   .state('y1','v1*sin(theta1)','m')  \
-   .state('theta1','-V/L*delta1','rad')\
-   .state('v1','a1','m/s')
+ocp.state('x','v*cos(theta)','m')   \
+   .state('y','v*sin(theta)','m')  \
+   .state('v','(a+1)/2','m/s') \
+   .state('theta','-v/L*delta','rad')
+# ocp.state('x1','v1*cos(theta1)','m')   \
+#    .state('y1','v1*sin(theta1)','m')  \
+#    .state('theta1','-V/L*delta1','rad')\
+#    .state('v1','(a1+1)/2','m/s')
 
 
 # Define quantities used in the problem
 
 # Define controls
+ocp.control('a','m/s^2')
 ocp.control('delta','rad')
-ocp.control('delta1','rad')
-ocp.control('a1','m/s^2')
+# ocp.control('delta1','rad')
+# ocp.control('a1','m/s^2')
 
 # Define constants
 ocp.constant('L', 1.0, 'm')
 ocp.constant('V', 1.0, 'm/s')
-ocp.constant('steeringLim', 30*pi/180, 'rad')
+ocp.constant('steeringLim', 40*pi/180, 'rad')
 # Define costs
 ocp.path_cost('1','s')
 
@@ -39,28 +41,19 @@ ocp.constraints() \
     .terminal('x-x_f','m')  \
     .terminal('y-y_f','m') \
     .terminal('theta-theta_f','rad') \
-    .initial('x1-x1_0','m')  \
-    .initial('y1-y1_0','m') \
-    .initial('v1-v1_0','m/s') \
-    .initial('theta1-theta1_0','rad') \
-    .terminal('x1-x1_f','m')  \
-    .terminal('y1-y1_f','m') \
-    .terminal('v1-v1_f','m/s') \
-    .terminal('theta1-theta1_f','rad') \
     .path('steering','delta','<>','steeringLim','rad', start_eps=1) \
-    .path('steering1','delta1','<>','steeringLim','rad', start_eps=1) \
-    .path('accLimit','a1','<>',1.0,'m/s^2',start_eps=1e-2)
+    .path('accLimit','a','<>',1.0,'m/s^2',start_eps=1)
 
 # ocp.scale(m='x', s='x/V', rad=1, nd=1)
 ocp.scale(m=1, s=1, rad=1, nd=1)
 
 guess_maker = beluga.guess_generator('auto',
-                start=[0,0,pi/4,0,10,-pi/4,0.1],
+                start=[0,0,0.1, pi/4],
                 direction='forward',
-                time_integrate=0.1,
-                costate_guess = 0.01,
-                control_guess = [0.0, 0.1, 0.1, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0],
-                use_control_guess=True
+                time_integrate=1.0,
+                costate_guess = -0.01,
+                control_guess = [0.1, 0.0, 0.1, 0.1, 0.1, 0.1],
+                use_control_guess=False
 )
 
 continuation_steps = beluga.init_continuation()
@@ -92,11 +85,11 @@ continuation_steps.add_step('bisection') \
 
 
 bvp_solver = beluga.bvp_algorithm('MultipleShooting',
-                        derivative_method='fd',
-                        tolerance=1e-4,
-                        max_iterations=25,
-                        verbose = True,
-                        max_error=100
+                derivative_method='fd',
+                tolerance=1e-4,
+                max_iterations=250,
+                verbose = True,
+                max_error=100
              )
 
 
