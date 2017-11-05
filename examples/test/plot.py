60af76257bbaeb96d46cbaf8f3c9e15933a91023
Thomas Antony <tantony@purdue.edu>
2017-11-04 23:36:56 -0500

    Two-vehicle zermelo with free constant velocity for second.

--- examples/test/plot.py
+++ examples/test/plot.py
@@ -2,20 +2,29 @@ from beluga.visualization import BelugaPlot
 import matplotlib.pyplot as plt
 plots = BelugaPlot('./data.dill',default_sol=-1,default_step=-1, renderer='matplotlib')
 
-# plots.add_plot().line('x*V*tfreal','y*V*tfreal')                    \
-#                 .xlabel('x(t)').ylabel('y(t)')      \
+# plots.add_plot().line('x*V*tfreal','y*V*tfreal')\
+#                 .xlabel('x(t)').ylabel('y(t)')\
 #                 .title('Trajectory')
-plots.add_plot().line('x','y',label='traj1')                    \
-                .xlabel('x(t)').ylabel('y(t)')      \
+plots.add_plot().line('x','y',label='traj1')\
+                .line('x2','y2',label='traj2')\
+                .xlabel('x(t)').ylabel('y(t)')\
                 .title('Trajectory') \
                 .postprocess(lambda a,b,c: plt.axis('equal'))
 
-plots.add_plot().line('t','theta*180/pi')                    \
-                .xlabel('t (s)').ylabel('theta [deg]')      \
+plots.add_plot().line('t','theta*180/pi')\
+                .xlabel('t (s)').ylabel('theta [deg]')\
                 .title('Control history')
 
-# plots.add_plot().line('t','sqrt((x - x2)**2 + (y - y2)**2)*(V*tfreal)')                    \
-#                 .xlabel('t').ylabel('Separation (m)')      \
+plots.add_plot().line('t','v2')\
+                .xlabel('t (s)').ylabel('v2 [deg]')\
+                .title('Control history')
+
+#
+# plots.add_plot().line('t','thr')\
+#                 .xlabel('t (s)').ylabel('thr [nd]')\
+#                 .title('Control history')
+# plots.add_plot().line('t','sqrt((x - x2)**2 + (y - y2)**2)*(V*tfreal)')\
+#                 .xlabel('t').ylabel('Separation (m)')\
 #                 .title('Separation')
 
 
--- examples/test/zermelo.py
+++ examples/test/zermelo.py
@@ -2,21 +2,35 @@ from math import pi
 
 ocp = beluga.OCP('zermelo1')
 ocp.independent('t', 's')
-ocp.state('x','V*cos(theta)','m')
-ocp.state('y','V*sin(theta)+x','m')
+# ocp.state('x','(thr+1)/2*V*cos(theta)','m')
+# ocp.state('y','(thr+1)/2*V*sin(theta)+x','m')
 
+ocp.state('x','V*cos(theta)+0','m')
+ocp.state('y','V*sin(theta)+x','m')
 ocp.control('theta','rad')
 
+ocp.state('x2','v2*cos(theta2)+0','m')
+ocp.state('y2','v2*sin(theta2)+x2','m')
+ocp.state('v2','0','m/s')
+ocp.control('theta2','rad')
+
 ocp.constant('V',10,'m/s')
-ocp.path_cost('1','s')
+ocp.path_cost('1 + theta**2 + theta2**2','s')
+
 ocp.constraints().initial('x-x_0','m')\
                 .initial('y-y_0','m')\
                 .terminal('x-x_f','m')\
                 .terminal('y-y_f','m')
 
-ocp.constraints().path('c1','y','>',-1,'nd',start_eps=1e-6)
+ocp.constraints().initial('x2-x2_0','m')\
+                 .initial('y2-y2_0','m')\
+                 .terminal('x2-x2_f','m')\
+                 .terminal('y2-y2_f','m')
+# ocp.constraints().path('c1','y','>',-1,'nd',start_eps=1e-6)
+                # .path('thr1','thr','<>',1,'nd',start_eps=1e-6)
+
 ocp.constant('xc',5,'m')
-ocp.constant('yc',10,'m')
+ocp.constant('yc',0.5,'m')
 ocp.constant('rc',0.1,'m')
 
 ocp.scale(m=1, s=1, kg=1, rad=1,nd=1)
@@ -24,7 +38,7 @@ ocp.scale(m=1, s=1, kg=1, rad=1,nd=1)
 bvp_solver = beluga.bvp_algorithm('MultipleShooting',
                         derivative_method='fd',
                         tolerance=1e-4,
-                        max_iterations=100,
+                        max_iterations=30,
                         verbose = True,
                         max_error=400,
              )
@@ -32,15 +46,24 @@ bvp_solver = beluga.bvp_algorithm('MultipleShooting',
 guess_maker = beluga.guess_generator('auto',
                 start=[0, 0],
                 direction='forward',
-                # costate_guess = -0.1,
-                costate_guess = [-0.1,-0.1,0.0]
+                costate_guess = -0.1,
+                # costate_guess = [-0.1,-0.1,0.0]
+)
+
+guess_maker = beluga.guess_generator('auto',
+                start=[0, 0, 0, 2, 10.0],
+                direction='forward',
+                costate_guess = -0.1,
+                # costate_guess = [-0.1,-0.1,0.0]
 )
 
 continuation_steps = beluga.init_continuation()
 
-continuation_steps.add_step('bisection').num_cases(5)           \
+continuation_steps.add_step('bisection').num_cases(11)           \
                         .terminal('x',10) \
-                        .terminal('y',0.0)
+                        .terminal('y',0.0) \
+                        .terminal('x2',10) \
+                        .terminal('y2',0.0)
 
 beluga.solve(ocp,
              method='icrm',
