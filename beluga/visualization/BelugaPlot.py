d430f56f4429407a79997f0b7d9dd5e1caaafcdf
Thomas Antony <tantony@purdue.edu>
2017-11-11 17:47:03 -0600

    Added support for multiple post-processing functions.

--- beluga/visualization/BelugaPlot.py
+++ beluga/visualization/BelugaPlot.py
@@ -79,8 +79,8 @@ class BelugaPlot:
             fig = self.renderer.create_figure()
             self.renderer.render_plot(fig, plot)
             self._figures.append(fig)
-            if plot.postprocess_fn is not None:
-                plot.postprocess_fn(self.renderer, fig, plot)
+            for fn in plot.postprocess_list:
+                fn(self.renderer, fig, plot)
         if show:
             self.show()
 
