2a234764e8ead3adf7c888782fc1587bfb386a21
Thomas Antony <tantony@purdue.edu>
2017-11-11 14:54:47 -0600

    Performed trades based on terminal heading.

--- examples/icrm/multi/twoveh-s1.py
+++ examples/icrm/multi/twoveh-s1.py
@@ -29,7 +29,7 @@ ocp.control('abar2','nd')
 ocp.control('gam2','nd')
 
 # Jammer location
-ocp.constant('xc',-0.6,'nd')
+ocp.constant('xc',-0.4,'nd')
 ocp.constant('yc',0.,'nd')
 ocp.constant('rc',.1,'nd')
 #
@@ -98,25 +98,29 @@ guess_maker = beluga.guess_generator('auto',
 )
 
 continuation_steps = beluga.init_continuation()
+#
+# continuation_steps.add_step('bisection') \
+#                 .num_cases(11) \
+#                 .terminal('xbar', -0.4)\
+#                 .terminal('ybar', -.2) \
+#                 .terminal('zbar', 0.) \
+#                 .terminal('psi', +15*pi/180) \
+#                 .terminal('xbar2', -.4)\
+#                 .terminal('ybar2', .2) \
+#                 .terminal('zbar2', 0.) \
+#                 .terminal('psi2', -15*pi/180) \
+#
+# continuation_steps.add_step('bisection') \
+#                 .num_cases(21) \
+#                 .terminal('xbar', 0.)\
+#                 .terminal('ybar', 0.) \
+#                 .terminal('xbar2', 0.)\
+#                 .terminal('ybar2', 0.)
 
+guess_maker = beluga.guess_generator('file',filename='data-twoveh-s1a.dill',iteration=-1,step=-1)
 continuation_steps.add_step('bisection') \
-                .num_cases(11) \
-                .terminal('xbar', -0.6)\
-                .terminal('ybar', -.25) \
-                .terminal('zbar', 0.) \
-                .terminal('psi', +15*pi/180) \
-                .terminal('xbar2', -.6)\
-                .terminal('ybar2', .25) \
-                .terminal('zbar2', 0.) \
-                .terminal('psi2', -15*pi/180) \
-
-continuation_steps.add_step('bisection') \
-                .num_cases(21) \
-                .terminal('xbar', 0.)\
-                .terminal('ybar', 0.) \
-                .terminal('xbar2', 0.)\
-                .terminal('ybar2', 0.)
-
+                .num_cases(41) \
+                .terminal('psi', +60*pi/180)
 
 beluga.solve(ocp,
              method='icrm',
