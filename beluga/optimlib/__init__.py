2f0886a911360577ef6b6009c208d683fed6f433
msparapa <msparapa@purdue.edu>
2018-07-10 16:35:42 -0500

    Ref #60 Committing some WIP

--- beluga/optimlib/__init__.py
+++ beluga/optimlib/__init__.py
@@ -2,7 +2,3 @@ from .brysonho import *
 from .optimlib import *
 from .icrm import *
 from .diffyg import *
-
-# from .brysonho import ocp_to_bvp
-
-methods = {'traditional': BrysonHo, 'icrm': ICRM, 'brysonho': BrysonHo}
