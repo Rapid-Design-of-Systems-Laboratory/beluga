e8693b996ac7ff5d96354fd40ffa3bd931c977ed
Michael Sparapany <msparapa@purdue.edu>
2019-02-06 15:56:19 -0500

    Ref #74 Removed visualization

--- beluga/visualization/renderers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .BaseRenderer import BaseRenderer
-from .MatPlotLib import MatPlotLib
-# from .ToyPlot import ToyPlot
-from .Bokeh import Bokeh
-
-import os
-import glob
-modules = glob.glob(os.path.dirname(__file__)+"/*.py")
-__all__ = [ os.path.basename(f)[:-3] for f in modules]
