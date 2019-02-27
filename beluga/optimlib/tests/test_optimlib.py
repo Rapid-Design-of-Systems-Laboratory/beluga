45507d891d91a39703848ab4c85f1372016ee7be
msparapa <msparapa@purdue.edu>
2019-02-27 12:58:30 -0500

    Ref #100 added a optimlib test case. WIP Do not merge.

--- beluga/optimlib/tests/test_optimlib.py
+++ beluga/optimlib/tests/test_optimlib.py
@@ -1,6 +1,70 @@
 import pytest
 from beluga.optimlib import *
 
+METHODS = ['traditional', 'icrm', 'direct']
+tol = 1e-8
+
+
+@pytest.mark.parametrize("method", METHODS)
+def test_composable_functors(method):
+    from beluga import OCP, ocp2bvp
+
+    problem = OCP('test_problem')
+    problem.independent('t', 's')
+    problem.state('x', 'v*cos(theta)', 'm')
+    problem.state('y', 'v*sin(theta)', 'm')
+    problem.state('v', 'g*sin(theta)', 'm/s')
+
+    problem.constant_of_motion('c1', 'lamX', 's/m')
+    problem.constant_of_motion('c2', 'lamY', 's/m')
+
+    problem.control('theta', 'rad')
+
+    problem.constant('g', -9.81, 'm/s^2')
+    problem.constant('x_f', 1, 'm')
+    problem.constant('y_f', -1, 'm')
+
+    problem.path_cost('1', '1')
+    problem.constraints().initial('x', 'm')
+    problem.constraints().initial('y', 'm')
+    problem.constraints().initial('v', 'm')
+    problem.constraints().terminal('x - x_f', 'm')
+    problem.constraints().terminal('y - y_f', 'm')
+
+    bvp, mapper, mapper_inv = ocp2bvp(problem, method)
+
+    gamma = Trajectory()
+    gamma.t = np.linspace(0, 1, num=10)
+    gamma.y = np.vstack([np.linspace(0, 0, num=10) for _ in range(3)]).T
+    gamma.dual = np.vstack([np.linspace(-1, -1, num=10) for _ in range(3)]).T
+    gamma.u = -np.pi / 2 * np.ones((10, 1))
+    gamma.const = np.array([-9.81, 1, -1])
+
+    g1 = mapper(gamma)
+    g2 = mapper_inv(g1)
+
+    assert g2.y.shape == gamma.y.shape
+    assert (g2.y - gamma.y < tol).all()
+
+    assert g2.q.shape == gamma.q.shape
+    assert (g2.q - gamma.q < tol).all()
+
+    assert g2.dual.shape == gamma.dual.shape
+    assert (g2.dual - gamma.dual < tol).all()
+
+    assert g2.u.shape == gamma.u.shape
+    assert (g2.u - gamma.u < tol).all()
+
+    assert g2.t.size == gamma.t.size
+    assert (g2.t - gamma.t < tol).all()
+
+    assert g2.dynamical_parameters.size == gamma.dynamical_parameters.size
+    assert (g2.dynamical_parameters - gamma.dynamical_parameters < tol).all()
+
+    assert g2.nondynamical_parameters.size == gamma.nondynamical_parameters.size
+    assert (g2.nondynamical_parameters - gamma.nondynamical_parameters < tol).all()
+
+
 def test_init_workspace():
     class emptyobj(object):
         def __new__(cls):
