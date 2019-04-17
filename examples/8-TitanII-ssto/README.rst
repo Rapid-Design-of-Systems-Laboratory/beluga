7e5efdec59191e46b1838386ad5a894fd5c0154e
msparapa <msparapa@purdue.edu>
2019-04-17 17:13:06 -0400

    Ref None added Titan II example

---
+++ examples/8-TitanII-ssto/README.rst
@@ -0,0 +1,14 @@
+.. |plot1| image:: https://cdn.jsdelivr.net/gh/Rapid-Design-of-Systems-Laboratory/beluga@master/examples/8-TitanII-ssto/TitanII1.png
+
+.. |plot2| image:: https://cdn.jsdelivr.net/gh/Rapid-Design-of-Systems-Laboratory/beluga@master/examples/8-TitanII-ssto/TitanII2.png
+
+.. |plot3| image:: https://cdn.jsdelivr.net/gh/Rapid-Design-of-Systems-Laboratory/beluga@master/examples/8-TitanII-ssto/TitanII3.png
+
+Low and High Thrust Orbit Raising
+=================================
+
+|plot1|
+
+|plot2|
+
+|plot3|
---
+++ examples/8-TitanII-ssto/TitanII.py
@@ -0,0 +1,120 @@
+import beluga
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+
+ocp = beluga.OCP('TitanII')
+
+# Define independent variables
+ocp.independent('t', 's')
+
+# Define equations of motion
+ocp.state('x', 'v_x', 'm') \
+   .state('y', 'v_y', 'm') \
+   .state('v_x', 'F/mass*cos(theta) - D/mass*v_x/sqrt(v_x**2 + v_y**2)', 'm/s') \
+   .state('v_y', 'F/mass*sin(theta) - D/mass*v_y/sqrt(v_x**2 + v_y**2) - g', 'm/s') \
+   .state('mass', 'md*eps', 'kg')
+
+# Define controls
+ocp.control('theta', 'rad')
+
+ocp.quantity('D', '1/2*rho_ref*exp(-y/Hscale)*CD*A*(v_x**2 + v_y**2)')
+
+# Define constants
+ocp.constant('F', 2.1e6, 'newton')
+ocp.constant('A', 7.069, 'm^2')
+ocp.constant('mu', 3.986004e14, 'm^3/s^2')
+ocp.constant('Re', 6378100, 'm')
+ocp.constant('CD', 0.5, '1')
+ocp.constant('rho_ref', 0, 'kg/m^3')
+ocp.constant('Hscale', 8.44e3, 'm')
+ocp.constant('g', 9.80665, 'm/s^2')
+ocp.constant('md', -807.6, 'kg/s')
+ocp.constant('eps', 0.000, '1')
+
+ocp.constant('x_0', 0, 'm')
+ocp.constant('y_0', 0, 'm')
+ocp.constant('v_x_0', 0, 'm/s')
+ocp.constant('v_y_0', 0.01, 'm/s')
+ocp.constant('mass_0', 60880, 'kg')
+
+ocp.constant('y_f', 1.8e5, 'm')
+ocp.constant('v_y_f', 0, 'm/s')
+
+# Define costs
+ocp.path_cost('1', '1')
+
+# Define constraints
+ocp.constraints() \
+    .initial('x - x_0', 'm')    \
+    .initial('y - y_0', 'm') \
+    .initial('v_x - v_x_0', 'm/s')  \
+    .initial('v_y - v_y_0', 'm/s')  \
+    .initial('mass - mass_0', 'kg') \
+    .terminal('y - y_f', 'm') \
+    .terminal('v_x - sqrt(mu/(y_f+Re))', 'm/s') \
+    .terminal('v_y - v_y_f', 'm/s')
+
+ocp.scale(m='y', s='y/v_x', kg='mass', newton='mass*v_x^2/y', rad=1)
+
+bvp_solver = beluga.bvp_algorithm('Shooting', algorithm='SLSQP')
+bvp_solver = beluga.bvp_algorithm('spbvp')
+
+guess_maker = beluga.guess_generator('auto',
+                start=[0, 0, 0, 0.01, 60880],          # Starting values for states in order
+                costate_guess = -0.1,
+                control_guess=[0],
+                use_control_guess=False
+)
+
+beluga.add_logger(logging_level=logging.DEBUG)
+
+sol_set = beluga.solve(ocp=ocp,
+             method='indirect',
+             bvp_algorithm=bvp_solver,
+             steps=None,
+             guess_generator=guess_maker, autoscale=True)
+
+guess_maker = beluga.guess_generator('static', solinit=sol_set[-1][-1])
+
+continuation_steps = beluga.init_continuation()
+
+continuation_steps.add_step('bisection') \
+                .num_cases(10) \
+                .const('eps', 1) \
+                .const('mass_0', 1.1702e5)
+
+continuation_steps.add_step('bisection') \
+                .num_cases(10) \
+                .const('rho_ref', 1.225)
+
+sol_set = beluga.solve(ocp=ocp,
+             method='indirect',
+             bvp_algorithm=bvp_solver,
+             steps=continuation_steps,
+             guess_generator=guess_maker, autoscale=True)
+
+sol = sol_set[-1][-1]
+
+plt.plot(sol.y[:,0]/1000, sol.y[:,1]/1000)
+plt.xlabel('Downrange [km]')
+plt.ylabel('Altitude [km]')
+plt.title('Time Optimal Launch of a Titan-II Trajectory')
+plt.grid('on')
+plt.show()
+
+plt.plot(sol.t, sol.y[:,2]/1000, label='Horizontal Velocity')
+plt.plot(sol.t, sol.y[:,3]/1000, label='Vertical Velocity')
+plt.xlabel('Time [s]')
+plt.ylabel('Velocity [km/s]')
+plt.title('Velocities of a Titan-II')
+plt.legend()
+plt.grid('on')
+plt.show()
+
+plt.plot(sol.t, sol.u*180/np.pi)
+plt.xlabel('Time [s]')
+plt.ylabel('Control [degrees]')
+plt.title('Titan-II Steering Angle')
+plt.grid('on')
+plt.show()
