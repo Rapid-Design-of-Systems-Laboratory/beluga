8d27cdaa78fbd8d7a79d48dd05ceae1eab06337b
Michael Sparapany <mjspara@sandia.gov>
2021-06-09 11:31:53 -0600

    Ref None adding python 3.9 classifier

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(name="beluga",
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Physics'
